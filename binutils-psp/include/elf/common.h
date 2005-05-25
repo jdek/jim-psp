029d47b0bdff4a9c11a9ff0ca372949fd4f01fd0
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-05-25 00:44:14 +0000

    Add the PSP .prx/.elf type and the Allegrex machine type.

--- binutils-psp/include/elf/common.h
+++ binutils-psp/include/elf/common.h
@@ -93,6 +93,7 @@
 #define ET_HIOS		0xFEFF	/* Operating system-specific */
 #define ET_LOPROC	0xFF00	/* Processor-specific */
 #define ET_HIPROC	0xFFFF	/* Processor-specific */
+#define ET_PSPEXEC	0xFFA0	/* Sony PSP executable file */
 
 /* Values for e_machine, which identifies the architecture.  These numbers
    are officially assigned by registry@caldera.com.  See below for a list of
