6b4fa00e85e3e52e10b01de00718664b904428b1
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-06-17 00:09:49 +0000

    Add Tim Styer's original Python script to split up matched words.

---
+++ libdoc/scripts/split_matches.py
@@ -0,0 +1,106 @@
+#!/bin/sh
+""""exec ${PYTHON:-python} -tu `cygpath -ma $0` "$@";" """
+
+import re, string
+
+re_nid = re.compile('<NID>(.*)</NID>')
+re_name = re.compile('<NAME>(.*)</NAME>')
+
+lowercase = string.lowercase
+uppercase = string.uppercase
+digits = string.digits
+
+
+def split_name (name):
+
+	splits = set()
+
+	# lowercase SPLIT uppercase
+	for i in xrange(len(name)-1):
+		if lowercase.find(name[i])>=0 and uppercase.find(name[i+1])>=0:
+			splits.add(i+1)
+
+	# SPLIT digit SPLIT
+	for i in xrange(len(name)):
+		if digits.find(name[i])>=0:
+			splits.add(i)
+			splits.add(i+1)
+
+	# uppercase SPLIT uppercase lowercase
+	for i in xrange(len(name)-2):
+		if uppercase.find(name[i])>=0 and uppercase.find(name[i+1])>=0 and lowercase.find(name[i+2])>=0:
+			splits.add(i+1)
+
+	# do the splitting
+	split_name = []
+	prev_split = 0
+	for split in xrange(1,len(name)):
+		if split in splits:
+			split_name.append(name[prev_split:split])
+			prev_split = split
+	split_name.append(name[prev_split:])
+	return split_name
+
+
+def has_uppercase_uppercase (name):
+
+	for i in xrange(0,len(name)-1):
+		if uppercase.find(name[i])>=0 and uppercase.find(name[i+1])>=0:
+			return True
+	return False
+
+
+def owiethohwetowehiroiwehrjoiwejr (split_name_list):
+
+	for i in xrange(len(split_name_list)):
+		for j in xrange(i,len(split_name_list)):
+			print ''.join(split_name_list[i:j+1])
+
+
+def main ():
+
+	file = open('psplibdoc.xml')
+
+	nid = None
+
+	for line in file:
+
+		line = line.strip()
+
+		result = re_nid.search(line)
+		if result:
+			nid = result.group(1)
+
+		result = re_name.search(line)
+		if result:
+			if nid:
+				name = result.group(1)
+				if name.find(nid[2:]) < 0:
+					words = split_name(name)
+#					print nid, name, words
+					owiethohwetowehiroiwehrjoiwejr(words)
+				nid = None
+
+	file.close()
+
+
+def test ():
+
+	names = [
+		'sceKernelUtilsMt19937Init',
+		'jklsajfdlsdfjdEEa',
+		'jklsajfdlsdfdEE2AAE',
+		'jklsajfdlsdfdEE2AaE',
+	]
+
+	for name in names:
+		words = split_name(name)
+		print name, words
+		owiethohwetowehiroiwehrjoiwejr(words)
+
+
+if __name__ == "__main__":
+
+	main()
+#	test()
+
