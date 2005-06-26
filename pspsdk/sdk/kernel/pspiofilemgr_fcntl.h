e0a7076fe65599d98477d9ecadf91e089ce21b0b
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-06-26 12:08:24 +0000

    Prepend PSP_ to the open flags and seek whences.  These values are not compatible with the standard Unix open flags and also caused a naming clash.

--- pspsdk/sdk/kernel/pspiofilemgr_fcntl.h
+++ pspsdk/sdk/kernel/pspiofilemgr_fcntl.h
@@ -1,38 +1,25 @@
 /*
-# _____     ___ ____     ___ ____
-#  ____|   |    ____|   |        | |____|
-# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
-#-----------------------------------------------------------------------
-# Copyright (c) 2003 Marcus R. Brown <mrbrown@0xd6.org>
-# Licenced under Academic Free License version 2.0
-# Review ps2sdk README & LICENSE files for further details.
-#
-# $Id$
-# File control definitions.
-*/
+ * $Id$
+ * File control definitions.
+ */
+#ifndef PSPIOFILEMGR_FCNTL_H
+#define PSPIOFILEMGR_FCNTL_H
 
-#ifndef SYS_FCNTL_H
-#define SYS_FCNTL_H
+/* Note: Not all of these sceIoOpen() flags are not compatible with the
+   open() flags found in sys/unistd.h. */
+#define PSP_O_RDONLY	0x0001
+#define PSP_O_WRONLY	0x0002
+#define PSP_O_RDWR	(PSP_O_RDONLY | PSP_O_WRONLY)
+#define PSP_O_NBLOCK	0x0004
+#define PSP_O_DIROPEN	0x0008	// Internal use for dopen
+#define PSP_O_APPEND	0x0100
+#define PSP_O_CREAT	0x0200
+#define PSP_O_TRUNC	0x0400
+#define	PSP_O_EXCL	0x0800
+#define PSP_O_NOWAIT	0x8000
 
-/* Hum... we have redundancy with io_common.h here */
+#define PSP_SEEK_SET	0
+#define PSP_SEEK_CUR	1
+#define PSP_SEEK_END	2
 
-#define O_RDONLY	0x0001
-#define O_WRONLY	0x0002
-#define O_RDWR		0x0003
-#define O_DIROPEN	0x0008	// Internal use for dopen
-#define O_NBLOCK	0x0010
-#define O_APPEND	0x0100
-#define O_CREAT		0x0200
-#define O_TRUNC		0x0400
-#define	O_EXCL		0x0800
-#define O_NOWAIT	0x8000
-
-// Access flags for filesystem mount
-#define FIO_MT_RDWR			0x00
-#define	FIO_MT_RDONLY		0x01
-
-#define SEEK_SET	0
-#define SEEK_CUR	1
-#define SEEK_END	2
-
-#endif /* SYS_FCNTL_H */
+#endif /* PSPIOFILEMGR_FCNTL_H */
