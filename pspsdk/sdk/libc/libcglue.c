b0ae3f02b0fe863333448ad6b9ae02daa5b7f4cf
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-06-26 11:30:02 +0000

    Add the libpspglue.a library that will be used to implement the "syscalls" that newlib requires.  Any overlap in libpsplibc.a will be removed, so that the user can choose either newlib's libc or pspsdk's libc.

---
+++ pspsdk/sdk/libc/libcglue.c
@@ -0,0 +1,96 @@
+
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+
+#include <psptypes.h>
+#include <psputils.h>
+
+/* Wrappers of the standard open(), close(), read(), write(), and lseek() routines. */
+#ifdef F__open
+#endif
+
+#ifdef F__close
+#endif
+
+#ifdef F__read
+#endif
+
+#ifdef F__write
+#endif
+
+#ifdef F__lseek
+#endif
+
+/* Time routines.  These wrap around the routines provided by the kernel. */
+#ifdef F__gettimeofday
+int _gettimeofday(struct timeval *tp, struct timezone *tzp)
+{
+	return sceKernelGettimeofday(tp, tzp);
+}
+#endif
+
+#ifdef F_clock
+clock_t clock(void)
+{
+	return sceKernelLibcClock();
+}
+#endif
+
+#ifdef F_time
+time_t time(time_t *t)
+{
+	return sceKernelLibcTime(t);
+}
+#endif
+
+/* PSP-compatible sbrk(). */
+#ifdef F__sbrk
+#endif
+
+/* Other POSIX routines that must be defined. */
+#ifdef F__fstat
+int fstat(int fd, struct stat *sbuf)
+{
+	sbuf->st_mode = 0x2000;
+	return 0;
+}
+#endif
+
+#ifdef F_isatty
+int isatty(int fd)
+{
+	return 1;
+}
+#endif
+
+/* Unsupported newlib system calls. */
+#ifdef F__fork
+pid_t fork(void)
+{
+	return (pid_t) -1;
+}
+#endif
+
+#ifdef F__getpid
+pid_t _getpid(void)
+{
+	return (pid_t) -1;
+}
+#endif
+
+#ifdef F__kill
+int _kill(int unused, int unused2)
+{
+	return -1;
+}
+#endif
+
+#ifdef F__wait
+pid_t _wait(int *unused)
+{
+	return (pid_t) -1;
+}
+#endif
