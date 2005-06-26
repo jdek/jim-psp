b2a2386e7e373e2d911a62a9f5c34c19e4178afb
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-06-26 21:05:27 +0000

    Add a pspmoduleinfo.h header which declares the SceModuleInfo struct and PSP_MDOULE_INFO() macro.  Remove the old MODULE_INFO() macro from pspkernel.h, and add pspmoduleinfo.h to pspkernel.h.

--- pspsdk/sdk/kernel/pspkernel.h
+++ pspsdk/sdk/kernel/pspkernel.h
@@ -3,20 +3,17 @@
 #ifndef __KERNEL_H__
 #define __KERNEL_H__
 
-/** Define the module info macro. This must be specified in one file of a project */
-#define MODULE_INFO(name, flags) asm ( \
-		".section \".rodata.sceModuleInfo\", \"a\", @progbits\r\n" 	\
-		".global  module_info\r\n" 						  	\
-		"module_info:\r\n"										\
-		".word " #flags "\r\n" 								  	\
-		".ascii \"" name "\"\r\n" 							  	\
-		".align 5\r\n"											\
-		".word _gp\r\n" 										\
-		".word __lib_ent_top\r\n"								\
-		".word __lib_ent_bottom\r\n"							\
-		".word __lib_stub_top\r\n"								\
-		".word __lib_stub_bottom\r\n"							\
-		);
+#include <psptypes.h>
+#include <pspctrl.h>
+#include <pspdisplay.h>
+#include <pspiofilemgr.h>
+#include <psploadexec.h>
+#include <pspmoduleinfo.h>
+#include <pspmodulemgr.h>
+#include <pspthreadman.h>
+#include <psputils.h>
+#include <pspumd.h>
+#include <pspge.h>
 
 /** @defgroup Kernel PSP Kernel Import Library */
 
@@ -34,20 +31,6 @@ extern const char* g_elf_name;
 extern int g_elf_namesize;
 
 /*@}*/
-
-#include <stddef.h>
-#include <stdarg.h>
-
-#include <psptypes.h>
-#include <pspctrl.h>
-#include <pspdisplay.h>
-#include <pspiofilemgr.h>
-#include <psploadexec.h>
-#include <pspmodulemgr.h>
-#include <pspthreadman.h>
-#include <psputils.h>
-#include <pspumd.h>
-#include <pspge.h>
 
 #ifdef __cplusplus
 }
---
+++ pspsdk/sdk/kernel/pspmoduleinfo.h
@@ -0,0 +1,78 @@
+/*
+ * pspmoduleinfo.h - Module definitions.
+ *
+ * $Id$
+ */
+#ifndef PSPMODULEINFO_H
+#define PSPMODULEINFO_H
+
+/* Note: Some of the structures and definitions in this file were extrapolated from
+   symbolic debugging information found in the Japanese version of Puzzle Bobble. */
+
+/* Module info structure.  Used to declare a module (library or executable).  This structure
+   is required in all PSP executables. */
+typedef struct _scemoduleinfo {
+	unsigned short		modattribute;
+	unsigned char		modversion[2];
+	char			modname[27];
+	char			terminal;
+	void *			gp_value;
+	void *			ent_top;
+	void *			ent_end;
+	void *			stub_top;
+	void *			stub_end;
+} _sceModuleInfo;
+
+typedef const _sceModuleInfo SceModuleInfo;
+
+extern char _gp[];
+
+/* Declare a module.  This must be specified in the source of a library or executable. */
+#define PSP_MODULE_INFO(name, attributes, major_version, minor_version) \
+	asm (                                                           \
+	"    .set push\n"                                               \
+	"    .section .lib.ent.top, \"a\", @progbits\n"                 \
+	"    .align 2\n"                                                \
+	"    .word 0\n"                                                 \
+	"__lib_ent_top:\n"                                              \
+	"    .section .lib.ent.btm, \"a\", @progbits\n"                 \
+	"    .align 2\n"                                                \
+	"__lib_ent_bottom:\n"                                           \
+	"    .word 0\n"                                                 \
+	"    .section .lib.stub.top, \"a\", @progbits\n"                \
+	"    .align 2\n"                                                \
+	"    .word 0\n"                                                 \
+	"__lib_stub_top:\n"                                             \
+	"    .section .lib.stub.btm, \"a\", @progbits\n"                \
+	"    .align 2\n"                                                \
+	"__lib_stub_bottom:\n"                                          \
+	"    .word 0\n"                                                 \
+	"    .set pop\n"                                                \
+	);                                                              \
+	SceModuleInfo module_info                                       \
+		__attribute__((section(".rodata.sceModuleInfo",         \
+			       aligned(16), unused))) = {               \
+	  attributes, { minor_version, major_version }, #name, 0, _gp,  \
+	  __lib_ent_top, __lib_ent_bottom,                              \
+	  __lib_stub_top, __lib_stub_bottom                             \
+	}
+
+/* This is TyRaNiD's original macro. */
+#if 0
+/** Define the module info macro. This must be specified in one file of a project */
+#define MODULE_INFO(name, flags) asm ( \
+		".section \".rodata.sceModuleInfo\", \"a\", @progbits\r\n" 	\
+		".global  module_info\r\n" 						  	\
+		"module_info:\r\n"										\
+		".word " #flags "\r\n" 								  	\
+		".ascii \"" name "\"\r\n" 							  	\
+		".align 5\r\n"											\
+		".word _gp\r\n" 										\
+		".word __lib_ent_top\r\n"								\
+		".word __lib_ent_bottom\r\n"							\
+		".word __lib_stub_top\r\n"								\
+		".word __lib_stub_bottom\r\n"							\
+		);
+#endif
+
+#endif /* PSPMODULEINFO_H */
