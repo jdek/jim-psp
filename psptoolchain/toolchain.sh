42e24328495c30c247e8e9522a43c068fff1777c
oopo <oopo@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-07-05 22:14:47 +0000

    Added pspsdk to the toolchain script, and made minor adjustments.

--- psptoolchain/toolchain.sh
+++ psptoolchain/toolchain.sh
@@ -8,8 +8,6 @@
 
   ## Main pspdev settings.
   PSPDEV="/usr/local/pspdev"
-  PSPSDK="$PSPDEV/pspsdk"
-  CVSROOT=":pserver:anonymous@cvs.ps2dev.org:/home/pspcvs"
   PATH="$PATH:$PSPDEV/bin"
 
   ## Set the directories.
@@ -21,44 +19,40 @@
   GCC="gcc-4.0.0"
   NEWLIB="newlib-1.13.0"
 
+ #########################
+ ## PARSE THE ARGUMENTS ##
+ #########################
 
-  ## Decide what to build
-  args=`getopt gbnx $*`
-  set -- $args
+  ## If no arguments are given, do everything.
+  if [ $# == 0 ] ; then
+   DO_DOWNLOAD=1
+   BUILD_BINUTILS=1
+   BUILD_GCC=1
+   BUILD_NEWLIB=1
+   BUILD_PSPSDK=1
 
-  for i
-  do
-    case "$i"
-    in
-      -g)
-        BUILD_GCC=1
-        shift;;
-      -b)
-        BUILD_BINUTILS=1
-        shift;;
-      -n)
-        BUILD_NEWLIB=1
-        shift;;
-      -x)
-        DO_NOT_DOWNLOAD=1
-        shift;;
+  ## Else, parse the arguments.
+  else
+   while [ $# -ge 1 ] ; do
+    case "$1" in
+     -d)
+      DO_DOWNLOAD=1
+      shift;;
+     -b)
+      BUILD_BINUTILS=1
+      shift;;
+     -g)
+      BUILD_GCC=1
+      shift;;
+     -n)
+      BUILD_NEWLIB=1
+      shift;;
+     -p)
+      BUILD_PSPSDK=1
+      shift;;
     esac
-  done
-
-  ## Download and build everything if nothing specified
-  if [ "$#" -lt "2" ]
-  then
-    BUILD_GCC=1
-    BUILD_BINUTILS=1
-    BUILD_NEWLIB=1
-    DO_NOT_DOWNLOAD=0
+   done
   fi
-
-  ## CVS configuration.
-  # if [ -z "`cat ~/.cvspass | grep $CVSROOT`" ]; then
-  #  echo "THE SECRET PASSWORD IS: anonymous"
-  #  cvs -d $CVSROOT login
-  # fi
 
  ###########################
  ## SOFTWARE DEPENDENCIES ##
@@ -76,32 +70,36 @@
   ## Check for wget.
   wget -V || { echo "ERROR: Please make sure you have 'wget' installed."; exit; }
 
+  ## Check for subversion.
+  svn help || { echo "ERROR: Please make sure you have 'subversion (svn)' installed."; exit; }
+
  ################################
  ## DOWNLOAD, UNPACK AND PATCH ##
  ################################
 
   ## Download the source.
-
-  if [ ! $DO_NOT_DOWNLOAD ] ; then
-    wget -c --passive-ftp ftp://ftp.gnu.org/pub/gnu/binutils/$BINUTILS.tar.gz
-    wget -c --passive-ftp ftp://ftp.gnu.org/pub/gnu/gcc/$GCC/$GCC.tar.bz2
-    wget -c --passive-ftp ftp://sources.redhat.com/pub/newlib/$NEWLIB.tar.gz
+  if [ $DO_DOWNLOAD ] ; then
+   wget -c --passive-ftp ftp://ftp.gnu.org/pub/gnu/binutils/$BINUTILS.tar.gz
+   wget -c --passive-ftp ftp://ftp.gnu.org/pub/gnu/gcc/$GCC/$GCC.tar.bz2
+   wget -c --passive-ftp ftp://sources.redhat.com/pub/newlib/$NEWLIB.tar.gz
   fi
 
-  ## Create the build directory.
+  ## Create the temp directory.
   mkdir -p $TMPDIR; cd $TMPDIR
 
-  ## Unpack and patch source files.
+  ## Unpack and patch the binutils source.
   if [ $BUILD_BINUTILS ] ; then
     rm -Rf $BINUTILS; tar xfvz $SRCDIR/$BINUTILS.tar.gz
     cd $BINUTILS; cat $SRCDIR/$BINUTILS.patch | patch -p1; cd ..
   fi
 
+  ## Unpack and patch the gcc source.
   if [ $BUILD_GCC ] ; then
     rm -Rf $GCC; tar xfvj $SRCDIR/$GCC.tar.bz2
     cd $GCC; cat $SRCDIR/$GCC.patch | patch -p1; cd ..
   fi
 
+  ## Unpack and patch the newlib source.
   if [ $BUILD_NEWLIB ] ; then
     rm -Rf $NEWLIB; tar xfvz $SRCDIR/$NEWLIB.tar.gz
     cd $NEWLIB; cat $SRCDIR/$NEWLIB.patch | patch -p1; cd ..
@@ -112,189 +110,151 @@
  ################################
 
   if [ $BUILD_BINUTILS ] ; then
-  
-    ## Enter the source directory.
-    cd $BINUTILS
-  
-    ## Build for each target.
-    for TARGET in "psp"; do
-  
-     ## Create the build directory.
-     mkdir build-$TARGET
-  
-     ## Enter the build directory.
-     cd build-$TARGET
-  
-     ## Configure the source.
-     ../configure --prefix=$PSPDEV --target=$TARGET --enable-install-libbfd || { echo "ERROR CONFIGURING BINUTILS ($BINUTILS $TARGET)"; exit; }
-  
-     ## Build the source.
-     $MAKE clean; $MAKE || { echo "ERROR BUILDING BINUTILS ($BINUTILS $TARGET)"; exit; }
-  
-     ## Install the result.
-     $MAKE install || { echo "ERROR INSTALLING BINUTILS ($BINUTILS $TARGET)"; exit; }
-  
-     ## Clean up the result.
-     $MAKE clean
-  
-     ## Exit the build directory.
-     cd ..
-  
-    ## End of the target build.
-    done
-  
-    ## Exit the source directory.
-    cd ..
+
+   ## Enter the source directory.
+   cd $BINUTILS
+
+   ## Create and enter the build directory.
+   mkdir build-psp; cd build-psp
+
+   ## Configure the source.
+   ../configure --prefix=$PSPDEV --target=psp --enable-install-libbfd || { echo "ERROR CONFIGURING BINUTILS"; exit; }
+
+   ## Build the source.
+   $MAKE clean; $MAKE || { echo "ERROR BUILDING BINUTILS"; exit; }
+
+   ## Install the result.
+   $MAKE install || { echo "ERROR INSTALLING BINUTILS"; exit; }
+
+   ## Clean up the result.
+   $MAKE clean
+
+   ## Exit the build and source directories.
+   cd ..; cd ..
 
   fi
-  
+
  ###########################
  ## BUILD AND INSTALL GCC ##
  ###########################
 
   if [ $BUILD_GCC ] ; then
-  
-    ## Enter the source directory.
-    cd $GCC
-  
-    ## Build for each target.
-    for TARGET in "psp"; do
-  
-     ## Create the build directory.
-     mkdir build-$TARGET
-  
-     ## Enter the build directory.
-     cd build-$TARGET
-  
-     ## Configure the source.
-     ../configure --prefix=$PSPDEV --target=$TARGET --enable-languages="c" --with-newlib --without-headers || { echo "ERROR CONFIGURING GCC ($GCC $TARGET)"; exit; }
-  
-     ## Build the source.
-     $MAKE clean; $MAKE || { echo "ERROR BUILDING GCC ($GCC $TARGET)"; exit; }
-  
-     ## Install the result.
-     $MAKE install || { echo "ERROR INSTALLING GCC ($GCC $TARGET)"; exit; }
-  
-     ## Clean up the result.
-     $MAKE clean
-  
-     ## Exit the build directory.
-     cd ..
-  
-    ## End of the target build.
-    done
-  
-    ## Exit the source directory.
-    cd ..
 
- fi
-  
+   ## Enter the source directory.
+   cd $GCC
+
+   ## Create and enter the build directory.
+   mkdir build-psp; cd build-psp
+
+   ## Configure the source.
+   ../configure --prefix=$PSPDEV --target=psp --enable-languages="c" --with-newlib --without-headers || { echo "ERROR CONFIGURING GCC"; exit; }
+
+   ## Build the source.
+   $MAKE clean; $MAKE || { echo "ERROR BUILDING GCC"; exit; }
+
+   ## Install the result.
+   $MAKE install || { echo "ERROR INSTALLING GCC"; exit; }
+
+   ## Clean up the result.
+   $MAKE clean
+
+   ## Exit the build and source directories.
+   cd ..; cd ..
+
+  fi
+
  ##############################
  ## BUILD AND INSTALL NEWLIB ##
  ##############################
 
   if [ $BUILD_NEWLIB ] ; then
-  
-    ## Enter the source directory.
-    cd $NEWLIB
-  
-    ## Build for each target.
-    for TARGET in "psp"; do
-  
-     ## Create the build directory.
-     mkdir build-$TARGET
-  
-     ## Enter the build directory.
-     cd build-$TARGET
-  
-     ## Configure the source.
-     ../configure --prefix=$PSPDEV --target=$TARGET || { echo "ERROR CONFIGURING NEWLIB ($NEWLIB $TARGET)"; exit; }
-  
-     ## Build the source.
-     $MAKE clean; $MAKE || { echo "ERROR BUILDING NEWLIB ($NEWLIB $TARGET)"; exit; }
-  
-     ## Install the result.
-     $MAKE install || { echo "ERROR INSTALLING NEWLIB ($NEWLIB $TARGET)"; exit; }
-  
-     ## Clean up the result.
-     $MAKE clean
-  
-     ## Exit the build directory.
-     cd ..
-  
-    ## End of the target build.
-    done
-  
-    ## Exit the source directory.
-    cd ..
 
- fi
-  
+   ## Enter the source directory.
+   cd $NEWLIB
+
+   ## Create and enter the build directory.
+   mkdir build-psp; cd build-psp
+
+   ## Configure the source.
+   ../configure --prefix=$PSPDEV --target=psp || { echo "ERROR CONFIGURING NEWLIB"; exit; }
+
+   ## Build the source.
+   $MAKE clean; $MAKE || { echo "ERROR BUILDING NEWLIB"; exit; }
+
+   ## Install the result.
+   $MAKE install || { echo "ERROR INSTALLING NEWLIB"; exit; }
+
+   ## Clean up the result.
+   $MAKE clean
+
+   ## Exit the build and source directories.
+   cd ..; cd ..
+
+  fi
+
  #################################
  ## BUILD AND INSTALL GCC (C++) ##
  #################################
 
   if [ $BUILD_GCC ] ; then
-  
-    ## Enter the source directory.
-    cd $GCC
-  
-    ## Build for each target.
-    for TARGET in "psp"; do
-  
-     ## Create the build directory.
-     mkdir build-$TARGET-c++
-  
-     ## Enter the build directory.
-     cd build-$TARGET-c++
-  
-     ## Configure the source.
-     ../configure --prefix=$PSPDEV --target=$TARGET --enable-languages="c,c++" --with-newlib --enable-cxx-flags="-G0" || { echo "ERROR CONFIGURING GCC ($GCC $TARGET C++)"; exit; }
-  
-     ## Build the source.
-     $MAKE clean; $MAKE CFLAGS_FOR_TARGET="-G0" || { echo "ERROR BUILDING GCC ($GCC $TARGET C++)"; exit; }
-  
-     ## Install the result.
-     $MAKE install || { echo "ERROR INSTALLING GCC ($GCC $TARGET C++)"; exit; }
-  
-     ## Clean up the result.
-     $MAKE clean
-  
-     ## Exit the build directory.
-     cd ..
-  
-    ## End of the target build.
-    done
-  
-    ## Exit the source directory.
-    cd ..
 
- fi
-  
+   ## Enter the source directory.
+   cd $GCC
+
+   ## Create and enter the build directory.
+   mkdir build-psp-c++; cd build-psp-c++
+
+   ## Configure the source.
+   ../configure --prefix=$PSPDEV --target=psp --enable-languages="c,c++" --with-newlib --enable-cxx-flags="-G0" || { echo "ERROR CONFIGURING GCC (C++)"; exit; }
+
+   ## Build the source.
+   $MAKE clean; $MAKE CFLAGS_FOR_TARGET="-G0" || { echo "ERROR BUILDING GCC (C++)"; exit; }
+
+   ## Install the result.
+   $MAKE install || { echo "ERROR INSTALLING GCC (C++)"; exit; }
+
+   ## Clean up the result.
+   $MAKE clean
+
+   ## Exit the build and source directories.
+   cd ..
+
+  fi
+
  ##############################
  ## BUILD AND INSTALL PSPSDK ##
  ##############################
 
-  ## Remove any previous builds.
-  # rm -Rf pspsdk
+  if [ $BUILD_PSPSDK ] ; then
 
-  ## Check out the latest source.
-  # cvs -d $CVSROOT checkout pspsdk
+   ## Remove any previous builds.
+   rm -Rf pspsdk
 
-  ## Enter the source directory.
-  # cd pspsdk
+   ## Check out the latest source.
+   svn checkout svn://svn.pspdev.org/psp/trunk/pspsdk
 
-  ## Build the source.
-  # $MAKE clean; $MAKE || { echo "ERROR BUILDING PSPSDK"; exit; }
+   ## Enter the source directory.
+   cd pspsdk
 
-  ## Install the result.
-  # $MAKE release || { echo "ERROR INSTALLING PSPSDK"; exit; }
-  
-  ## Clean up the result.
-  # $MAKE clean
+   ## Bootstrap the source.
+   ./bootstrap || { echo "ERROR RUNNING PSPSDK BOOSTRAP"; exit; }
 
-  ## Exit the source directory.
-  # cd ..
+   ## Configure the source.
+   ./configure || { echo "ERROR RUNNING PSPSDK CONFIGURE"; exit; }
+
+   ## Build the source.
+   $MAKE clean; $MAKE || { echo "ERROR BUILDING PSPSDK"; exit; }
+
+   ## Install the result.
+   $MAKE install || { echo "ERROR INSTALLING PSPSDK"; exit; }
+
+   ## Clean up the result.
+   $MAKE clean
+
+   ## Exit the source directory.
+   cd ..
+
+  fi
 
  #########################
  ## CLEAN UP THE RESULT ##
@@ -310,4 +270,4 @@
   rm -Rf $NEWLIB
 
   ## Clean up pspsdk.
-  # rm -Rf pspsdk
+  rm -Rf pspsdk
