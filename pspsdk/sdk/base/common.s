069ab3e28600cc1700624723a7bbfb7b277f5901
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-07-09 08:21:21 +0000

    Cleanup base/common.s: use the official section names for stubs and NIDs used in the linker script.  Make sure common.s is included in the distribution.

--- pspsdk/sdk/base/common.s
+++ pspsdk/sdk/base/common.s
@@ -1,46 +1,59 @@
-	.macro	STUB_START	module,d1,d2
+# common.s - GAS macros for creating import libraries.
+#
+# Use STUB_START to declare the start of an import library.  The module parameter
+# is the name of the library to import, flags_ver is both the import flags and library
+# version, and stub_len is both the number of stubs to import and the size of the
+# stub itself (in 32-bit words).
+# Use the STUB_FUNC macro for each stub you want to import.  The funcid parameter is
+# the 32-bit SHA1-derived NID value, and the funcname is the name you want to call
+# the imported function.
+# Use STUB_END to declare the end of your import library.
 
-		.section	.rodata.stubmodulename,"a"
-		.word	0
+.macro STUB_START module, flags_ver, stub_len
+
+	.set push
+	.section .rodata.sceResident, "a"
+	.word   0
 __stub_modulestr_\module:
-		.asciz	"\module"
-		.align	2
+	.asciz  "\module"
+	.align  2
 
-		.section	.lib.stub,"wa",@progbits
-		.word __stub_modulestr_\module
-		.word \d1
-		.word \d2
-		.word __stub_idtable_\module
-		.word __stub_text_\module
+	.section .lib.stub, "a", @progbits
+	.word   __stub_modulestr_\module
+	.word   \flags_ver
+	.word   \stub_len
+	.word   __stub_idtable_\module
+	.word   __stub_text_\module
 
-		.section	.rodata.stubidtable,"a"
+	.section .rodata.sceNid, "a"
 __stub_idtable_\module:
 
-		.section	.text.stub,"a",@progbits
+	.section .sceStub.text, "ax", @progbits
 __stub_text_\module:
 
-	.endm
+	.set pop
+.endm
 
+.macro STUB_FUNC funcid, funcname
 
-	.macro	STUB_END
-	.endm
+	.set push
+	.set noreorder
 
-
-	.macro	STUB_FUNC	funcid,funcname
-
-		.set push
-		.set noreorder
-
-		.section	.text.stub
-		.weak	\funcname
+	.section .sceStub.text, "ax", @progbits
+	.globl  \funcname
+	.type   \funcname, @function
+	.ent    \funcname, 0
 \funcname:
-		jr	$ra
-		nop
+	jr	$ra
+	nop
+	.end    \funcname
+	.size   \funcname, .-\funcname
 
-		.section	.rodata.stubidtable
-		.word	\funcid
+	.section .rodata.sceNid
+	.word   \funcid
 
-		.set pop
+	.set pop
+.endm
 
-	.endm
-
+.macro STUB_END
+.endm
