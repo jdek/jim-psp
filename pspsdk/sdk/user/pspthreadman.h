548d6df21e985b6181cab85439e342bbfa3844c9
warren <warren@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-07-13 22:54:00 +0000

    Added more functions to pspthreadman.h

--- pspsdk/sdk/user/pspthreadman.h
+++ pspsdk/sdk/user/pspthreadman.h
@@ -545,6 +545,26 @@ int sceKernelCreateEventFlag(const char *name, int unk1, int bits, int unk3);
   */
 int sceKernelSetEventFlag(int evid, u32 bits);
 
+/** 
+  * Poll an event flag for a giveb bit pattern.
+  *
+  * @param evid - The event id returned by sceKernelCreateEventFlag.
+  * @param bits - The bit pattern to poll for.
+  * @param wait - 0 to return immediately, 1 to wait until the pattern is matched
+  * @param outBits - The bit pattern that was matched.
+  * @return < 0 On error
+  */
+int sceKernelPollEventFlag(int evid, u32 bits, u32 wait, u32 *outBits);
+
+/** 
+  * Delete an event flag
+  *
+  * @param evid - The event id returned by sceKernelCreateEventFlag.
+  *
+  * @return < 0 On error
+  */
+int sceKernelDeleteEventFlag(int evid);
+
 /*@}*/
 
 #ifdef __cplusplus
