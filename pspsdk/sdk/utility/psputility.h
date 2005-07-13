043f251270dbb805d4db1f1330325715c5544579
warren <warren@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-07-13 06:57:42 +0000

    Added sceUtility*NetConParam and related constants Added utility/netconf sample (should be cleaned up a lot) Split psputility.h into psputility_sysparam.h and psputility_netconf.h to make things more legible

--- pspsdk/sdk/utility/psputility.h
+++ pspsdk/sdk/utility/psputility.h
@@ -3,7 +3,7 @@
  * -----------------------------------------------------------------------
  * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
  *
- *  psputility.h - Prototypes for the pspUtility library
+ *  psputility.h - Master include for the pspUtility library
  *
  * Copyright (c) 2005 John Kelley <ps2dev@kelley.ca>
  *
@@ -12,123 +12,7 @@
 #ifndef __PSPUTILITY_H__
 #define __PSPUTILITY_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/**
- * IDs for use inSystemParam functions
- * PSP_SYSTEMPARAM_ID_INT are for use with SystemParamInt funcs
- * PSP_SYSTEMPARAM_ID_STRING are for use with SystemParamString funcs
- */
-#define PSP_SYSTEMPARAM_ID_STRING_NICKNAME	1
-#define PSP_SYSTEMPARAM_ID_INT_ADHOC_CHANNEL	2
-#define PSP_SYSTEMPARAM_ID_INT_WLAN_POWERSAVE	3
-#define PSP_SYSTEMPARAM_ID_INT_DATE_FORMAT	4
-#define PSP_SYSTEMPARAM_ID_INT_TIME_FORMAT	5
-#define PSP_SYSTEMPARAM_ID_INT_TIMEZONE		6 //like hell I'm mapping these out (-300 = GMT-5 EST)
-#define PSP_SYSTEMPARAM_ID_INT_DAYLIGHTSAVINGS	7
-#define PSP_SYSTEMPARAM_ID_INT_LANGUAGE		8
-/**
- * #9 seems to be Region or maybe X/O button swap.
- * It doesn't exist on JAP v1.0
- * is 1 on NA v1.5s
- * is 0 on JAP v1.5s
- * is read-only
- */
-#define PSP_SYSTEMPARAM_ID_INT_UNKNOWN		9
-
-/**
- * Return values for the SystemParam functions
- */
-#define PSP_SYSTEMPARAM_RETVAL_OK	0
-#define PSP_SYSTEMPARAM_RETVAL_FAIL	0x80110103
-
-/**
- * Valid values for PSP_SYSTEMPARAM_ID_INT_ADHOC_CHANNEL
- */
-#define PSP_SYSTEMPARAM_ADHOC_CHANNEL_AUTOMATIC 0
-#define PSP_SYSTEMPARAM_ADHOC_CHANNEL_1		1
-#define PSP_SYSTEMPARAM_ADHOC_CHANNEL_6		6
-#define PSP_SYSTEMPARAM_ADHOC_CHANNEL_11	11
-
-/**
- * Valid values for PSP_SYSTEMPARAM_ID_INT_WLAN_POWERSAVE
- */
-#define PSP_SYSTEMPARAM_WLAN_POWERSAVE_OFF	0
-#define PSP_SYSTEMPARAM_WLAN_POWERSAVE_ON	1
-
-/**
- * Valid values for PSP_SYSTEMPARAM_ID_INT_DATE_FORMAT
- */
-#define PSP_SYSTEMPARAM_DATE_FORMAT_YYYYMMDD	0
-#define PSP_SYSTEMPARAM_DATE_FORMAT_MMDDYYYY	1
-#define PSP_SYSTEMPARAM_DATE_FORMAT_DDMMYYYY	2
-
-/**
- * Valid values for PSP_SYSTEMPARAM_ID_INT_TIME_FORMAT
- */
-#define PSP_SYSTEMPARAM_TIME_FORMAT_24HR	0
-#define PSP_SYSTEMPARAM_TIME_FORMAT_12HR	1
-
-/**
- * Valid values for PSP_SYSTEMPARAM_ID_INT_DAYLIGHTSAVINGS
- */
-#define PSP_SYSTEMPARAM_DAYLIGHTSAVINGS_STD	0
-#define PSP_SYSTEMPARAM_DAYLIGHTSAVINGS_SAVING	1
-
-/**
- * Valid values for PSP_SYSTEMPARAM_ID_INT_LANGUAGE
- */
-#define PSP_SYSTEMPARAM_LANGUAGE_JAPANESE	0
-#define PSP_SYSTEMPARAM_LANGUAGE_ENGLISH	1
-#define PSP_SYSTEMPARAM_LANGUAGE_FRENCH		2
-#define PSP_SYSTEMPARAM_LANGUAGE_SPANISH	3
-#define PSP_SYSTEMPARAM_LANGUAGE_GERMAN		4
-#define PSP_SYSTEMPARAM_LANGUAGE_ITALIAN	5
-#define PSP_SYSTEMPARAM_LANGUAGE_DUTCH		6
-#define PSP_SYSTEMPARAM_LANGUAGE_PORTUGUESE	7
-#define PSP_SYSTEMPARAM_LANGUAGE_KOREAN		8
-
-/**
- * Set Integer System Parameter
- *
- * @param id - which parameter to set
- * @param value - integer value to set
- * @returns 0 on success, PSP_SYSTEMPARAM_RETVAL_FAIL on failure
- */
-int sceUtilitySetSystemParamInt(int id, int value);
-
-/**
- * Set String System Parameter
- *
- * @param id - which parameter to set
- * @param str - char * value to set
- * @returns 0 on success, PSP_SYSTEMPARAM_RETVAL_FAIL on failure
- */
-int sceUtilitySetSystemParamString(int id, const char *str);
-
-/**
- * Get Integer System Parameter
- *
- * @param id - which parameter to get
- * @param value - pointer to integer value to place result in
- * @returns 0 on success, PSP_SYSTEMPARAM_RETVAL_FAIL on failure
- */
-int sceUtilityGetSystemParamInt( int id, int *value );
-
-/**
- * Get String System Parameter
- *
- * @param id - which parameter to get
- * @param str - char * buffer to place result in
- * @param len - length of str buffer
- * @returns 0 on success, PSP_SYSTEMPARAM_RETVAL_FAIL on failure
- */
-int sceUtilityGetSystemParamString(int id, char *str, int len);
-
-#ifdef __cplusplus
-}
-#endif
+#include <psputility_sysparam.h>
+#include <psputility_netconf.h>
 
 #endif
---
+++ pspsdk/sdk/utility/psputility_netconf.h
@@ -0,0 +1,77 @@
+/*
+ * PSP Software Development Kit - http://www.pspdev.org
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ *  psputility_netconf.h - Definitions and Functions for Network Configuration 
+ *                         section of the pspUtility library
+ *
+ * Copyright (c) 2005 John Kelley <ps2dev@kelley.ca>
+ *
+ * $Id: psputility.h 581 2005-07-11 03:47:07Z warren $
+ */
+#ifndef __PSPUTILITY_NETCONF_H__
+#define __PSPUTILITY_NETCONF_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <psptypes.h>
+
+/**
+ * Datatype for sceUtilityGetNetParam
+ * since it can return a u32 or a string
+ * we use a union to avoid ugly casting
+ */
+typedef union {
+	u32 asUint;
+	char asString[128];
+} netData;
+
+#define PSP_NETPARAM_NAME          0 // string
+#define PSP_NETPARAM_SSID          1 // string
+#define PSP_NETPARAM_SECURE        2 // int
+#define PSP_NETPARAM_WEPKEY        3 // string
+#define PSP_NETPARAM_IS_STATIC_IP  4 // int
+#define PSP_NETPARAM_IP            5 // string
+#define PSP_NETPARAM_NETMASK       6 // string
+#define PSP_NETPARAM_ROUTE         7 // string
+#define PSP_NETPARAM_MANUAL_DNS    8 // int
+#define PSP_NETPARAM_PRIMARYDNS    9 // string
+#define PSP_NETPARAM_SECONDARYDNS 10 // string
+#define PSP_NETPARAM_PROXY_USER   11 // string
+#define PSP_NETPARAM_PROXY_PASS   12 // string
+#define PSP_NETPARAM_USE_PROXY    13 // int
+#define PSP_NETPARAM_PROXY_SERVER 14 // string
+#define PSP_NETPARAM_PROXY_PORT   15 // int
+#define PSP_NETPARAM_UNKNOWN1     16 // int
+#define PSP_NETPARAM_UNKNOWN2     17 // int
+
+#define PSP_NETPARAM_ERROR_BAD_NETCONF	0x80110601
+#define PSP_NETPARAM_ERROR_BAD_PARAM	0x80110604
+
+/**
+ * Check existance of a Net Configuration
+ *
+ * @param id - id of net Configuration (1 to n)
+ * @returns 0 on success, 
+ */
+int sceUtilityCheckNetParam(int id);
+
+/**
+ * Get Net Configuration Parameter
+ *
+ * @param conf - Net Configuration number (1 to n)
+ * (0 returns valid but seems to be a copy of the last config requested)
+ * @param param - which parameter to get
+ * @param data - parameter data
+ * @returns 0 on success, 
+ */
+int sceUtilityGetNetParam(int conf, int param, netData *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
