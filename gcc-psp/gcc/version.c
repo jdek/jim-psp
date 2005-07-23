ace7db8d878b016292f2bc6814e4690f8e981e89
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-07-23 04:37:09 +0000

    Add a specific version and bug-reporting URL to GCC.

--- gcc-psp/gcc/version.c
+++ gcc-psp/gcc/version.c
@@ -5,7 +5,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "4.0.1";
+const char version_string[] = "4.0.1 (PSPDEV 20050722)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
@@ -14,4 +14,4 @@ const char version_string[] = "4.0.1";
    forward us bugs reported to you, if you determine that they are
    not bugs in your modifications.)  */
 
-const char bug_report_url[] = "<URL:http://gcc.gnu.org/bugs.html>";
+const char bug_report_url[] = "<URL:http://wiki.pspdev.org/psp:toolchain#bugs>";
