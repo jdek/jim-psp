9184f7e7ae0f7cf773e0771a6dbf466c65b837f2
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-08-20 00:16:07 +0000

    Might work now :/

--- pspsdk/tools/_stdint.h
+++ pspsdk/tools/_stdint.h
@@ -3,7 +3,7 @@
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "prxgen 0.1.0"
 /* generated using gnu compiler gcc (GCC) 3.3.5 (Debian 1:3.3.5-8ubuntu2) */
-#define _STDINT_HAVE_STDINT_H 1
+#define HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
 #endif
