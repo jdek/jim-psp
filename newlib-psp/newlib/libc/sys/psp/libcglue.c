f7545c55c041040373acd30882700f286507c90c
jim <jim@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-10-05 08:45:53 +0000

    Added access() to newlib.

--- newlib-psp/newlib/libc/sys/psp/libcglue.c
+++ newlib-psp/newlib/libc/sys/psp/libcglue.c
@@ -518,8 +518,19 @@ pid_t _wait(int *unused)
 #endif
 
 #ifdef F_access
-int access(const char *pathname, int mode)
+int access(const char *fn, int flags)
 {
+	struct stat s;
+	if (stat(fn, &s))
+		return -1;
+	if (s.st_mode & S_IFDIR)
+		return 0;
+	if (flags & W_OK)
+	{
+		if (s.st_mode & S_IWRITE)
+			return 0;
+		return -1;
+	}
 	return 0;
 }
 #endif
