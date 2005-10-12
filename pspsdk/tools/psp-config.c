09da1c0d2a1ae3b06e66e0040ac0249b3bfdf2f9
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-10-12 19:32:01 +0000

    Added path normalisation from anhanguera

--- pspsdk/tools/psp-config.c
+++ pspsdk/tools/psp-config.c
@@ -86,6 +86,22 @@ void print_help(void)
 	fprintf(stderr, "-P, --psp-prefix        : Print the prefix of PSP-hosted software\n");
 }
 
+void normalize_path (char *out)
+{
+       int i;
+       int j;
+
+       /* Convert "//" to "/" */
+       for (i = 0; out[i + 1]; i++) {
+               if (out[i] == '/' && out[i + 1] == '/') {
+                       for (j = i + 1; out[j]; j++) {
+                               out[j] = out[j + 1];
+                       }
+                       i--;
+               }
+       }
+}
+
 /* Find the path to the pspdev dir (e.g. /usr/local/pspdev) */
 char *find_pspdev_path(char *name)
 {
@@ -166,6 +182,8 @@ char *find_pspdev_path(char *name)
 			}
 		}
 	}
+
+	normalize_path(path);
 
 	if(found)
 	{
