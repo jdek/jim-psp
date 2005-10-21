8a30935d774f62bb6ff8187440582fd5d22eb7bb
chip <chip@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-10-21 23:49:30 +0000

    cleaned up envmap-sample

--- pspsdk/sdk/samples/gu/envmap/envmap.c
+++ pspsdk/sdk/samples/gu/envmap/envmap.c
@@ -20,6 +20,7 @@
 #include <pspctrl.h>
 
 #include <pspgu.h>
+#include <pspgum.h>
 
 PSP_MODULE_INFO("EnvMap Sample", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
@@ -39,17 +40,6 @@ int SetupCallbacks();
 
 void genTorus( unsigned slices, unsigned rows, float radius, float thickness,
 	Vertex* dstVertices, unsigned short* dstIndices );
-
-void matrix_identity(float* matrix);
-void matrix_rotate(float* matrix, float x, float y, float z);
-void matrix_projection(float* matrix, float fovy, float aspect, float near, float far);
-void matrix_multiply(float* result, float* a, float* b);
-void matrix_setrotatex(float* matrix, float angle);
-void matrix_setrotatey(float* matrix, float angle);
-void matrix_setrotatez(float* matrix, float angle);
-void matrix_rotate(float* matrix, float x, float y, float z);
-void matrix_translate(float* matrix, float x, float y, float z);
-void matrix_fastinverse(float* dest, float* source);
 
 #define BUF_WIDTH (512)
 #define SCR_WIDTH (480)
@@ -116,10 +106,6 @@ int main(int argc, char* argv[])
 
 	// run sample
 
-	ScePspFMatrix4 projection;
-	ScePspFMatrix4 view;
-	ScePspFMatrix4 world;
-
 	int valTorus = 0;
 	int valEnvMap = 0;
 	int rotateTorus = 1;
@@ -145,12 +131,12 @@ int main(int argc, char* argv[])
 
 		// setup proj/view matrices
 
-		matrix_identity((float*)&projection);
-		matrix_projection((float*)&projection,75.0f,16.0/9.0f,1.0f,1000.0f);
-		sceGuSetMatrix(GU_PROJECTION,&projection);
+		sceGumMatrixMode(GU_PROJECTION);
+		sceGumLoadIdentity();
+		sceGumPerspective(75.0f,16.0/9.0f,1.0f,1000.0f);
 
-		matrix_identity((float*)&view);
-		sceGuSetMatrix(GU_VIEW,&view);
+		sceGumMatrixMode(GU_VIEW);
+		sceGumLoadIdentity();
 
 		// setup envmap texture
 
@@ -187,13 +173,17 @@ int main(int argc, char* argv[])
 
 		// draw torus
 
-		matrix_identity((float*)&world);
-		matrix_translate((float*)&world,0,0,-2.5f);
-		matrix_rotate((float*)&world,valTorus * 0.79f * (M_PI/180.0f), valTorus * 0.98f * (M_PI/180.0f), valTorus * 1.32f * (M_PI/180.0f));
-		sceGuSetMatrix(GU_MODEL,&world);
+		sceGumMatrixMode(GU_MODEL);
+		{
+			ScePspFVector3 pos = {0,0,-2.5f};
+			ScePspFVector3 rot = {valTorus * 0.79f * (M_PI/180.0f), valTorus * 0.98f * (M_PI/180.0f), valTorus * 1.32f * (M_PI/180.0f)};
+			sceGumLoadIdentity();
+			sceGumTranslate(&pos);
+			sceGumRotateXYZ(&rot);
+		}
 
 		sceGuColor(0xffffff);
-		sceGuDrawArray(GU_TRIANGLES,GU_NORMAL_32BITF|GU_VERTEX_32BITF|GU_INDEX_16BIT|GU_TRANSFORM_3D,sizeof(torus_indices)/sizeof(unsigned short),torus_indices,torus_vertices);
+		sceGumDrawArray(GU_TRIANGLES,GU_NORMAL_32BITF|GU_VERTEX_32BITF|GU_INDEX_16BIT|GU_TRANSFORM_3D,sizeof(torus_indices)/sizeof(unsigned short),torus_indices,torus_vertices);
 
 		pspDebugScreenSetXY(0,0);
 		printf( "Press X to toggle environment map rotation" );
@@ -306,187 +296,4 @@ void genTorus( unsigned slices, unsigned rows, float radius, float thickness, Ve
 			*curr++ = i + j1 * rows;
 		}
 	}
-}
-
-/* small matrix library */
-
-void matrix_identity(float* matrix)
-{
-	matrix[(0<<2)+0] = 1.0f;
-	matrix[(0<<2)+1] = 0.0f;
-	matrix[(0<<2)+2] = 0.0f;
-	matrix[(0<<2)+3] = 0.0f;
-
-	matrix[(1<<2)+0] = 0.0f;
-	matrix[(1<<2)+1] = 1.0f;
-	matrix[(1<<2)+2] = 0.0f;
-	matrix[(1<<2)+3] = 0.0f;
-
-	matrix[(2<<2)+0] = 0.0f;
-	matrix[(2<<2)+1] = 0.0f;
-	matrix[(2<<2)+2] = 1.0f;
-	matrix[(2<<2)+3] = 0.0f;
-
-	matrix[(3<<2)+0] = 0.0f;
-	matrix[(3<<2)+1] = 0.0f;
-	matrix[(3<<2)+2] = 0.0f;
-	matrix[(3<<2)+3] = 1.0f;
-}
-
-void matrix_projection(float* matrix, float fovy, float aspect, float near, float far)
-{
-	matrix_identity(matrix);
-
-	float angle = (fovy / 2.0f) * (M_PI/180.0f);
-	float cotangent = cosf(angle) / sinf(angle);
-
-	matrix[(0<<2)+0] = cotangent / aspect;
-	matrix[(1<<2)+1] = cotangent;
-	matrix[(2<<2)+2] = (far + near) / (near - far);
-	matrix[(3<<2)+2] = 2.0f * (far * near) / (near - far);
-	matrix[(2<<2)+3] = -1;
-	matrix[(3<<2)+3] = 0.0f;
-}
-
-void matrix_multiply(float* result, float* a, float* b)
-{
-	unsigned int i,j,k;
-	float temp[16];
-
-	for (i = 0; i < 4; ++i)
-	{
-		for (j = 0; j < 4; ++j)
-		{
-			float t = 0.0f;
-			for (k = 0; k < 4; ++k)
-				t += a[(k << 2)+j] * b[(i << 2)+k];
-			temp[(i << 2)+j] = t;
-		}
-	}
-
-	memcpy(result,temp,sizeof(float)*16);
-}
-
-void matrix_translate(float* matrix, float x, float y, float z)
-{
-	float temp[16];
-
-	matrix_identity(temp);
-	temp[(3 << 2)+0] = x;
-	temp[(3 << 2)+1] = y;
-	temp[(3 << 2)+2] = z;
-
-	matrix_multiply(matrix,matrix,temp);
-}
-
-void matrix_setrotatex(float* matrix, float angle)
-{
-	float cs = cosf(angle);
-	float sn = sinf(angle);
-
-	matrix_identity(matrix);
-	matrix[(1<<2)+1] = cs;
-	matrix[(1<<2)+2] = sn;
-	matrix[(2<<2)+1] = -sn;
-	matrix[(2<<2)+2] = cs;
-}
-
-void matrix_setrotatey(float* matrix, float angle)
-{
-	float cs = cosf(angle);
-	float sn = sinf(angle);
-
-	matrix_identity(matrix);
-	matrix[(0<<2)+0] = cs;
-	matrix[(0<<2)+2] = -sn;
-	matrix[(2<<2)+0] = sn;
-	matrix[(2<<2)+2] = cs;
-}
-
-void matrix_setrotatez(float* matrix, float angle)
-{
-	float cs = cosf(angle);
-	float sn = sinf(angle);
-
-	matrix_identity(matrix);
-	matrix[(0<<2)+0] = cs;
-	matrix[(0<<2)+1] = sn;
-	matrix[(1<<2)+0] = -sn;
-	matrix[(1<<2)+1] = cs;
-}
-
-void matrix_rotate(float* matrix, float x, float y, float z)
-{
-	float temp[16];
-
-	matrix_setrotatex(temp,x);
-	matrix_multiply(matrix,matrix,temp);
-
-	matrix_setrotatey(temp,y);
-	matrix_multiply(matrix,matrix,temp);
-
-	matrix_setrotatez(temp,z);
-	matrix_multiply(matrix,matrix,temp);
-}
-
-#define SIN_ITERATOR 20
-
-float sinf(float v)
-{
-	float res,w;
-	int t;
-	float fac;
-	int i=(int)((v)/(2.0f*M_PI));
-	v-=i*2.0f*M_PI;
-
-	fac=1.0f;
-	res=0.0f;
-	w=v;
-	for(t=1;t<SIN_ITERATOR;)
-	{
-		res+=fac*w;
-		w*=v*v;
-		t++;
-		fac/=t;
-		t++;
-		fac/=t;
-
-		res-=fac*w;
-		w*=v*v;
-		t++;
-		fac/=t;
-		t++;
-		fac/=t;
-	}
-	return res;
-}
-
-float cosf(float v)
-{
-	float res,w;
-	int t;
-	float fac;
-	int i=(int)((v)/(2.0f*M_PI));
-	v-=i*2.0f*M_PI;
-
-	fac=1.0f;
-	res=0.0f;
-	w=1.0f;
-	for(t=0;t<SIN_ITERATOR;)
-	{
-		res+=fac*w;
-		w*=v*v;
-		t++;
-		fac/=t;
-		t++;
-		fac/=t;
-
-		res-=fac*w;
-		w*=v*v;
-		t++;
-		fac/=t;
-		t++;
-		fac/=t;
-	}
-	return res;
 }
