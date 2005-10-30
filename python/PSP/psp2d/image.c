8968ede4ee16203389eefadc526aa08f75cbf946
fraca7 <fraca7@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-10-30 13:46:32 +0000

    PNG image support

--- python/PSP/psp2d/image.c
+++ python/PSP/psp2d/image.c
@@ -13,6 +13,8 @@
 #include "image.h"
 #include "color.h"
 
+#include <png.h>
+
 static void image_dealloc(PyImage *self)
 {
     if (self->data)
@@ -51,6 +53,16 @@ static int image_init(PyImage *self,
     // If only 1 argument was given, assume it is a filename.
     if (!b)
     {
+       // Code mostly taken from the libpng PSP example.
+
+       char *filename;
+       FILE *fp;
+       png_structp pptr;
+       png_infop iptr;
+       unsigned int sread = 0;
+       png_uint_32 w, h, j;
+       int depth, ctype, itype;
+
 #ifdef CHECKTYPE
        if (!PyString_Check(a))
        {
@@ -59,7 +71,83 @@ static int image_init(PyImage *self,
        }
 #endif
 
-       // TODO: load image from file...
+       filename = PyString_AsString(a);
+       fp = fopen(filename, "rb");
+       if (!fp)
+       {
+          PyErr_SetString(PyExc_IOError, "Could not open file");
+          return -1;
+       }
+
+       pptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+                                     NULL, NULL, NULL);
+       if (!pptr)
+       {
+          fclose(fp);
+
+          PyErr_SetString(PyExc_RuntimeError, "Error creating PNG struct");
+          return -1;
+       }
+
+       iptr = png_create_info_struct(pptr);
+       if (!iptr)
+       {
+          fclose(fp);
+          png_destroy_read_struct(&pptr, png_infopp_NULL, png_infopp_NULL);
+
+          PyErr_SetString(PyExc_RuntimeError, "Error creating PNG info struct");
+          return -1;
+       }
+
+       png_init_io(pptr, fp);
+       png_set_sig_bytes(pptr, sread);
+       png_read_info(pptr, iptr);
+       png_get_IHDR(pptr, iptr, &w, &h, &depth, &ctype, &itype,
+                    int_p_NULL, int_p_NULL);
+
+       if ((w > 512) || (h > 512))
+       {
+          fclose(fp);
+          png_destroy_read_struct(&pptr, png_infopp_NULL, png_infopp_NULL);
+
+          PyErr_SetString(PyExc_ValueError, "Width or height is > 512");
+          return -1;
+       }
+
+       png_set_strip_16(pptr);
+       png_set_packing(pptr);
+
+       if (ctype == PNG_COLOR_TYPE_PALETTE)
+          png_set_palette_to_rgb(pptr);
+
+       if (ctype == PNG_COLOR_TYPE_GRAY && depth < 8)
+          png_set_gray_1_2_4_to_8(pptr);
+
+       if (png_get_valid(pptr, iptr, PNG_INFO_tRNS))
+          png_set_tRNS_to_alpha(pptr);
+
+       png_set_filler(pptr, 0xff, PNG_FILLER_AFTER);
+
+       self->width = w;
+       self->height = h;
+       self->data = (u32*)malloc(sizeof(u32) * w * h);
+       if (!self->data)
+       {
+          fclose(fp);
+          png_destroy_read_struct(&pptr, png_infopp_NULL, png_infopp_NULL);
+
+          PyErr_SetString(PyExc_MemoryError, "Memory error");
+          return -1;
+       }
+
+       for (j = 0; j < h; ++j)
+       {
+          png_read_row(pptr, (u8*)(self->data + j * w), png_bytep_NULL);
+       }
+
+       png_read_end(pptr, iptr);
+       png_destroy_read_struct(&pptr, &iptr, png_infopp_NULL);
+       fclose(fp);
     }
     else
     {
@@ -86,6 +174,11 @@ static int image_init(PyImage *self,
 #endif
 
        self->data = (u32*)malloc(sizeof(u32) * w * h);
+       if (!self->data)
+       {
+          PyErr_SetString(PyExc_MemoryError, "Memory error");
+          return -1;
+       }
     }
 
     return 0;
