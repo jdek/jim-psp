567a30a1daf4a1c48d102b459899d58fad3d57f6
mrbrown <mrbrown@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-12-08 05:31:13 +0000

    Remove the weak attribute; since the BSD-compatible sockets API added to Newlib were written around these functions, it doesn't make sense for someone to not link them into their program.

--- pspsdk/src/net/pspnet_inet.h
+++ pspsdk/src/net/pspnet_inet.h
@@ -22,7 +22,6 @@ extern "C" {
 #endif
 
 int sceNetInetInit(void);
-
 int sceNetInetTerm(void);
 
 /* Define functions, make recv, send and close weak to avoid
@@ -32,15 +31,15 @@ int	sceNetInetBind(int s, const struct sockaddr *my_addr, socklen_t addrlen);
 int	sceNetInetConnect(int s, const struct sockaddr *serv_addr, socklen_t addrlen);
 int	sceNetInetGetsockopt(int s, int level, int optname, void *optval, socklen_t *optlen);
 int	sceNetInetListen(int s, int backlog);
-size_t	sceNetInetRecv(int s, void *buf, size_t len, int flags) __attribute__((weak));
+size_t	sceNetInetRecv(int s, void *buf, size_t len, int flags);
 size_t	sceNetInetRecvfrom(int s, void *buf, size_t flags, int, struct sockaddr *from, socklen_t *fromlen);
-size_t	sceNetInetSend(int s, const void *buf, size_t len, int flags) __attribute__((weak));
+size_t	sceNetInetSend(int s, const void *buf, size_t len, int flags);
 size_t	sceNetInetSendto(int s, const void *buf, size_t len, int flags, const struct sockaddr *to, socklen_t tolen);
 int	sceNetInetSetsockopt(int s, int level, int optname, const void *optval, socklen_t optlen);
 int	sceNetInetShutdown(int s, int how);
 int	sceNetInetSocket(int domain, int type, int protocol);
-int sceNetInetClose(int s) __attribute__((weak));
-int sceNetInetGetErrno(void) __attribute__((weak));
+int sceNetInetClose(int s);
+int sceNetInetGetErrno(void);
 
 #ifdef __cplusplus
 }
