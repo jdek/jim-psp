d2796134f7ff9ca6f588afb06e46582f663a148f
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-12-11 15:36:15 +0000

    Added some kernel only functions.

---
+++ pspsdk/src/kernel/pspsysmem_kernel.h
@@ -0,0 +1,76 @@
+/*
+ * PSP Software Development Kit - http://www.pspdev.org
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * pspsysmem_kernel.h - Interface to the system memory manager (kernel).
+ *
+ * Copyright (c) 2005 James F.
+ *
+ * $Id: pspsysmem.h 1095 2005-09-27 21:02:16Z jim $
+ */
+
+/* Note: Some of the structures, types, and definitions in this file were
+   extrapolated from symbolic debugging information found in the Japanese
+   version of Puzzle Bobble. */
+
+#ifndef PSPSYSMEMKERNEL_H
+#define PSPSYSMEMKERNEL_H
+
+#include <pspkerneltypes.h>
+#include <pspsysmem.h>
+
+/** @defgroup SysMemKern System Memory Manager Kernel
+  * This module contains routines to manage heaps of memory.
+  */
+
+/** @addtogroup SysMemKern System Memory Manager Kernel */
+/*@{*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _PspSysmemPartitionInfo
+{
+	SceSize size;
+	unsigned int startaddr;
+	unsigned int size;
+	unsigned int attr;
+} PspSysmemPartitionInfo;
+
+/**
+ * Query the parition information
+ *
+ * @param pid  - The partition id
+ * @param info - Pointer to the 
+ *
+ * @returns 0 on success.
+ */
+int sceKernelQueryMemoryPartitionInfo(int pid, PspSysmemPartitionInfo *info);
+
+/**
+ * Get the total amount of free memory.
+ *
+ * @param pid - The partition id
+ *
+ * @returns The total amount of free memory, in bytes.
+ */
+SceSize sceKernelPartitionTotalFreeMemSize(int pid);
+
+/**
+ * Get the size of the largest free memory block.
+ *
+ * @param pid - The partition id
+ *
+ * @returns The size of the largest free memory block, in bytes.
+ */
+SceSize sceKernelPartitionMaxFreeMemSize(int pid);
+
+#ifdef __cplusplus
+}
+#endif
+
+/*@}*/
+
+#endif /* PSPSYSMEMKERNEL_H */
