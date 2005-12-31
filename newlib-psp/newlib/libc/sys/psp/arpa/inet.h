12df89d80f79da456b68cef26c29e435a2aaf91f
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2005-12-31 00:18:12 +0000

    Fixed arpa/inet.h so it works correctly in C++

--- newlib-psp/newlib/libc/sys/psp/arpa/inet.h
+++ newlib-psp/newlib/libc/sys/psp/arpa/inet.h
@@ -1,5 +1,12 @@
 /* arpa/inet.h - Functions for converting IP addresses between strings and numbers */
 
+#ifndef _ARPA_INET_H_
+#define _ARPA_INET_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <netinet/in.h>
 
 in_addr_t sceNetInetInetAddr(const char *ip);
@@ -12,3 +19,8 @@ char *inet_ntoa(struct in_addr in);
 #define inet_aton sceNetInetInetAton
 #define inet_ntop sceNetInetInetNtop
 #define inet_pton sceNetInetInetPton
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _ARPA_INET_H_ */
