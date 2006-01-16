611f76a5781bed2625f9eb505d753da59d3aba92
chip <chip@fd6133d2-eef9-0310-a487-e40d4489b40b>
2006-01-16 10:45:23 +0000

    Added small debug-menu for GU samples, will be used by upcoming samples

---
+++ pspsdk/src/samples/gu/common/menu.h
@@ -0,0 +1,53 @@
+#ifndef common_menu_h
+#define common_menu_h
+
+#include <pspctrl.h>
+
+typedef enum
+{
+	MenuContainer,
+	RadioButton,
+	ToggleButton,
+	TriggerButton
+} MenuItemType;
+
+typedef struct MenuItem
+{
+	const char* name;
+	MenuItemType type;
+	int id;
+	int data;
+
+	int state;
+
+	struct MenuItem* parent;
+	struct MenuItem* next;
+	struct MenuItem* children;
+
+	// for radio-button parents
+
+	struct MenuItem* selected;
+} MenuItem;
+
+typedef struct MenuContext
+{
+	int open;
+	SceCtrlData lastState;
+
+	MenuItem* root;
+	MenuItem* active;
+} MenuContext;
+
+// initialize menu for use
+MenuContext* initMenu();
+// destroy menu
+void destroyMenu(MenuContext* context);
+// add menu-item into tree
+MenuItem* addMenuItem(MenuContext* context, MenuItem* parent, const char* name, MenuItemType type, int id, int data);
+// process menu input
+// - kills input while menu is open, pass the input-structure you intend to use with the rest of input
+// - returns triggered MenuItem
+MenuItem* handleMenu(MenuContext* context, SceCtrlData* input);
+void renderMenu(MenuContext* context,int startx, int starty);
+
+#endif
