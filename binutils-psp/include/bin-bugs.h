3ef232291582c57e02f871f58c37207aa224514b
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2006-01-22 14:04:06 +0000

    Tagged off beta2

--- binutils-psp/include/bin-bugs.h
+++
@@ -1,3 +0,0 @@
-#ifndef REPORT_BUGS_TO
-#define REPORT_BUGS_TO	"<URL:http://wiki.pspdev.org/psp:toolchain#bugs>"
-#endif
--- pspgl/tests/perfmeter.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PERFMETER_H
-#define _PERFMETER_H
-
-void pm_framestart(void);
-void pm_frameend(void);
-
-#endif /* _PERFMETER_H */
