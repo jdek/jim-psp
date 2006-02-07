8c39039ecd42c517542f9e8bdeceaa410e5b6137
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2006-02-07 22:31:44 +0000

    Added a library template.

---
+++ pspsdk/src/samples/template/lib_template/template.c
@@ -0,0 +1,18 @@
+/*
+ * PSP Software Development Kit - http://www.pspdev.org
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * template.c - Template source file for a library
+ *
+ * Copyright (c) 2005 James F <tyranid@gmail.com>
+ *
+ * $Id$
+ */
+
+#include "template.h"
+
+int template_call(void)
+{
+	return 0;
+}
