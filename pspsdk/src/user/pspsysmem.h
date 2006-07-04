dfb60c316186a9fff67284c06f76d21ccfb3248f
jim <jim@fd6133d2-eef9-0310-a487-e40d4489b40b>
2006-07-04 04:14:56 +0000

    add more devkit versions

--- pspsdk/src/user/pspsysmem.h
+++ pspsdk/src/user/pspsysmem.h
@@ -91,13 +91,15 @@ SceSize sceKernelMaxFreeMemSize(void);
  * Get the firmware version.
  * 
  * @returns The firmware version.
- * 0x01000300 on v1.00 unit
- * 0x01050001 on v1.50 unit
- * 0x01050100 on v1.51 unit
- * 0x01050200 on v1.52 unit
- * 0x02000010 on v2.00/v2.01 unit
- * 0x02050010 on v2.50 unit
- * 0x02060010 on v2.60 unit
+ * 0x01000300 on v1.00 unit,
+ * 0x01050001 on v1.50 unit,
+ * 0x01050100 on v1.51 unit,
+ * 0x01050200 on v1.52 unit,
+ * 0x02000010 on v2.00/v2.01 unit,
+ * 0x02050010 on v2.50 unit,
+ * 0x02060010 on v2.60 unit,
+ * 0x02070010 on v2.70 unit,
+ * 0x02070110 on v2.71 unit.
  */
 int sceKernelDevkitVersion(void);
 
