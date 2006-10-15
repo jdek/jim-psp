0a56f161f4767ffa2ad2a06d3b5d551d76182625
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2006-10-15 22:56:42 +0000

    Added heap functions from moonlight

--- pspsdk/src/kernel/pspsysmem_kernel.h
+++ pspsdk/src/kernel/pspsysmem_kernel.h
@@ -93,6 +93,56 @@ void sceKernelSysMemDumpTail(void);
  */
 int sceKernelSetDdrMemoryProtection(void *addr, int size, int prot);
 
+/**
+ * Create a heap.
+ *
+ * @param partitionid - The UID of the partition where allocate the heap.
+ * @param size - The size in bytes of the heap.
+ * @param unk - Unknown, probably some flag or type, pass 1.
+ * @param name - Name assigned to the new heap.
+ *
+ * @returns The UID of the new heap, or if less than 0 an error. 
+*/
+SceUID sceKernelCreateHeap(SceUID partitionid, SceSize size, int unk, const char *name);
+
+/**
+ * Allocate a memory block from a heap.
+ *
+ * @param heapid - The UID of the heap to allocate from.
+ * @param size - The number of bytes to allocate.
+ *
+ * @returns The address of the allocated memory block, or NULL on error.
+*/
+void *sceKernelAllocHeapMemory(SceUID heapid, SceSize size);
+
+/**
+ * Free a memory block allocated from a heap.
+ *
+ * @param heapid - The UID of the heap where block belongs.
+ * @param block - The block of memory to free from the heap.
+ *
+ * @returns 0 on success, < 0 on error.
+ */
+int sceKernelFreeHeapMemory(SceUID heapid, void *block);
+
+/**
+ * Delete a heap.
+ *
+ * @param heapid - The UID of the heap to delete.
+ *
+ * @returns 0 on success, < 0 on error.
+*/
+int sceKernelDeleteHeap(SceUID heapid);
+
+/**
+ * Get the amount of free size of a heap, in bytes.
+ *
+ * @param heapid - The UID of the heap
+ *
+ * @returns the free size of the heap, in bytes. < 0 on error.
+*/
+SceSize sceKernelHeapTotalFreeSize(SceUID heapid);
+
 #ifdef __cplusplus
 }
 #endif
