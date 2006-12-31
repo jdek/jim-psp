4780f41739cf2ba3368c788b4554ac549c9f67c3
holger <holger@fd6133d2-eef9-0310-a487-e40d4489b40b>
2006-12-31 11:51:47 +0000

    implement glutGetProcAddress() and export all symbols (more than the spec wants for eglGetProcAddress(), required for glutGetProcAddress())

--- pspgl/eglGetProcAddress.c
+++ pspgl/eglGetProcAddress.c
@@ -1,27 +1,34 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "pspgl_internal.h"
 
 struct pspgl_proc {
 	const char *name;
-	void	(*proc)();
+	void (*proc) (void);
 };
 
-static const struct pspgl_proc proctable[] = {
-#include "pspgl_proctable.h"
-};
-#define NPROC	(sizeof(proctable)/sizeof(proctable[0]))
 
-static int cmp_procname(const void *a, const void *b)
+#include "pspgl_proctable_decls.h"
+
+static const
+struct pspgl_proc proctable[] = {
+	#include "pspgl_proctable.h"
+};
+
+#define NPROC (sizeof(proctable)/sizeof(proctable[0]))
+
+
+static
+int cmp_procname(const void *a, const void *b)
 {
 	const char *name = a;
 	const struct pspgl_proc *entry = b;
-
 	return strcmp(name, entry->name);
 }
 
-GLAPI void (* APIENTRY eglGetProcAddress (const char *procname))()
+void (* eglGetProcAddress (const char *procname)) ();
+
+void (* eglGetProcAddress (const char *procname)) ()
 {
 	struct pspgl_proc *entry;
 
