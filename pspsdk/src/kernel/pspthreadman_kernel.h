10dd23d22148dcee6e43c3ea4e92eeb29c709b2b
tyranid <tyranid@fd6133d2-eef9-0310-a487-e40d4489b40b>
2007-01-09 00:29:48 +0000

    Added prototype for kernel version of refer thread status (1.5 version only)

--- pspsdk/src/kernel/pspthreadman_kernel.h
+++ pspsdk/src/kernel/pspthreadman_kernel.h
@@ -129,6 +129,100 @@ void *sceKernelGetKTLS(int id);
  */
 void *sceKernelGetThreadKTLS(int id, SceUID thid, int mode);
 
+/** Thread context
+ * Structues for the thread context taken from florinsasu's post on the forums */
+struct SceThreadContext {
+	unsigned int   type;
+	unsigned int   gpr[31];
+	unsigned int   fpr[32];
+	unsigned int   fc31;
+	unsigned int   hi; 
+	unsigned int   lo;
+	unsigned int   SR;
+	unsigned int   EPC;
+	unsigned int   field_114;
+	unsigned int   field_118;
+}; 
+
+struct SceSCContext
+{
+	unsigned int status;
+	unsigned int epc;
+	unsigned int sp;
+	unsigned int ra;
+	unsigned int k1;
+	unsigned int unk[3];
+};
+
+/** Structure to hold the status information for a thread (kernel form)
+ * 1.5 form
+  */
+typedef struct SceKernelThreadKInfo {
+	/** Size of the structure */
+	SceSize     size;
+	/** Nul terminated name of the thread */
+	char    	name[32];
+	/** Thread attributes */
+	SceUInt     attr;
+	/** Thread status */
+	int     	status;
+	/** Thread entry point */
+	SceKernelThreadEntry    entry;
+	/** Thread stack pointer */
+	void *  	stack;
+	/** Thread stack size */
+	int     	stackSize;
+	/** Kernel stack pointer */
+	void *		kstack;
+	/** Kernel stack size */
+	void *		kstackSize;
+	/** Pointer to the gp */
+	void *  	gpReg;
+	/** Size of args */
+	SceSize     args;
+	/** Pointer to args */
+	void *      argp;
+	/** Initial priority */
+	int     	initPriority;
+	/** Current priority */
+	int     	currentPriority;
+	/** Wait type */
+	int     	waitType;
+	/** Wait id */
+	SceUID  	waitId;
+	/** Wakeup count */
+	int     	wakeupCount;
+	/** Number of clock cycles run */
+	SceKernelSysClock   runClocks;
+	/* SceUInt unk3; Unknown extra field on 3.X */
+	/** Interrupt preemption count */
+	SceUInt     intrPreemptCount;
+	/** Thread preemption count */
+	SceUInt     threadPreemptCount;
+	/** Release count */
+	SceUInt     releaseCount;
+	/** Thread Context */
+	struct SceThreadContext *thContext;
+	/** VFPU Context */
+	float *      vfpuContext;
+	/** Return address from syscall */
+	void  *      retAddr;
+	/** Unknown, possibly size of SC context */
+	SceUInt      unknown1;
+	/** Syscall Context */
+	struct SceSCContext *scContext;
+} SceKernelThreadKInfo;
+
+/**
+ * Refer kernel version of thread information
+ *
+ * @param uid - UID to find
+ * @param info - Pointer to info structure, ensure size is set before calling
+ *
+ * @return 0 on success
+ */
+int ThreadManForKernel_2D69D086(SceUID uid, SceKernelThreadKInfo *info);
+
 #ifdef __cplusplus
 }
 #endif
