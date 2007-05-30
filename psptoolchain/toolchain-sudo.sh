cd9ab37fadf05042160345dadb60c44a2b6bc7d3
oopo <oopo@fd6133d2-eef9-0310-a487-e40d4489b40b>
2007-05-30 20:34:49 +0000

    Added a script that can build the toolchain under sudo. Made some other minor script tweaks.

---
+++ psptoolchain/toolchain-sudo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# toolchain-sudo.sh by Dan Peori (danpeori@oopo.net)
+
+ ## Enter the script directory.
+ cd `dirname $0` || { echo "ERROR: Could not enter the script directory."; exit 1; }
+
+ ## Set up the environment.
+ export PSPDEV=/usr/local/pspdev
+ export PATH=$PATH:$PSPDEV/bin
+
+ ## Run the toolchain script.
+ ./toolchain.sh || { echo "ERROR: Could not run the toolchain script."; exit 1; }
