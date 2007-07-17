1b5d5ac02a5ab0cfd7281a58fafc977f6441c168
oopo <oopo@fd6133d2-eef9-0310-a487-e40d4489b40b>
2007-07-17 18:18:05 +0000

    Added better detection of ncurses.a and ncurses.dll.a.

--- psptoolchain/depends/check-ncurses.sh
+++ psptoolchain/depends/check-ncurses.sh
@@ -2,4 +2,4 @@
 # check-ncurses.sh by Dan Peori (danpeori@oopo.net)
 
  ## Check for a ncurses library.
- ls /usr/lib/libncurses.* 1> /dev/null || { echo "ERROR: Install ncurses before continuing."; exit 1; }
+ ls /usr/lib/libncurses.a 1> /dev/null || ls /usr/lib/libncurses.dll.a || { echo "ERROR: Install ncurses before continuing."; exit 1; }
