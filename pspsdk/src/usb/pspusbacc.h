dc3c2d01ee0a7485c7b69f174d1711089e39c94e
raphael <raphael@fd6133d2-eef9-0310-a487-e40d4489b40b>
2007-12-06 21:49:26 +0000

    Added pspjpeg and pspusbcam stubs and headers, following code from dot_blank.

---
+++ pspsdk/src/usb/pspusbacc.h
@@ -0,0 +1,26 @@
+/*
+ * PSP Software Development Kit - http://www.pspdev.org
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * pspusbacc.h - Prototypes for the sceUsbAcc library
+ *
+ * Copyright (c) 2007 dot_blank
+ *
+ * $Id$
+ */
+#ifndef __PSPUSBACC_H__
+#define __PSPUSBACC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PSP_USBACC_DRIVERNAME	"USBAccBaseDriver"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
+
