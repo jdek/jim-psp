6c9122bfbcfcd1f1e3f1a162fc447510aadafaa3
iwn <iwn@fd6133d2-eef9-0310-a487-e40d4489b40b>
2008-01-06 21:48:44 +0000

    Updated psputility_savedata.h as discussed here: http://forums.ps2dev.org/viewtopic.php?t=9457 and based on my own findings. Thanks to johnmph for additional information. samples/savedata/utility updated to reflect the changes and added actual dialog functionality.

--- pspsdk/src/samples/savedata/utility/main.c
+++ pspsdk/src/samples/savedata/utility/main.c
@@ -6,189 +6,364 @@
  * main.c - Sample to demonstrate how to use savedata utility
  *
  * Copyright (c) 2005 weltall (weltall@consoleworld.org)
+ *			 (c) 2007 InsertWittyName (tias_dp@hotmail.com)
  * Based on work by Shine
  *
  * $Id$
  */
 #include <pspkernel.h>
 #include <pspdisplay.h>
-#include <pspctrl.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include <pspmoduleinfo.h>
-#include <pspiofilemgr.h>
 #include <pspdebug.h>
+#include <pspgu.h>
+#include <pspgum.h>
 #include <psputility.h>
+#include <pspctrl.h>
 
-/* Define the module info section */
-PSP_MODULE_INFO("Savedata Sample", 0, 1, 0);
+#include "data.h"
 
-/* Define printf, just to make typing easier */
-#define printf  pspDebugScreenPrintf
+PSP_MODULE_INFO("Savedata Sample", 0, 1, 1);
 
-/* Game information.  This example would place savedata in
-   the directory /PSP/SAVEDATA/DEMO111110000, with the 
-   encrypted data being stored in the file SAVE.BIN. */
-char *gameName = "DEMO11111";
-char *saveName = "0000";
-char *fileName = "SAVE.BIN";
+#if _PSP_FW_VERSION >= 200
+PSP_HEAP_SIZE_KB(20480);
+#endif
 
-/* define for buffer length */
+#define BUF_WIDTH (512)
+#define SCR_WIDTH (480)
+#define SCR_HEIGHT (272)
+#define PIXEL_SIZE (4) /* change this if you change to another screenmode */
+#define FRAME_SIZE (BUF_WIDTH * SCR_HEIGHT * PIXEL_SIZE)
+#define ZBUF_SIZE (BUF_WIDTH SCR_HEIGHT * 2) /* zbuffer seems to be 16-bit? */
 
-#define DATABUFFLEN   0x100000
-#define READICON0LEN  0x100000
-#define READICON1LEN  0x100000
-#define READPIC1LEN   0x100000
+#define DATABUFFLEN   0x20
 
-/* Exit callback */
+static int running = 1;
+
+char string[128];
+
+// Exit callback
 int exit_callback(int arg1, int arg2, void *common)
 {
-	sceKernelExitGame();
-	return 0;
+    running = 0;
+	
+    return 0;
 }
 
-/* Callback thread */
+// Callback thread
 int CallbackThread(SceSize args, void *argp)
 {
-	int cbid;
-	cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-	sceKernelRegisterExitCallback(cbid);
-	sceKernelSleepThreadCB();
+    int cbid;
+    cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+    sceKernelRegisterExitCallback(cbid);
+    sceKernelSleepThreadCB();
 
-	return 0;
+    return 0;
 }
 
-/* Sets up the callback thread and returns its thread id */
+// Sets up the callback thread and returns its thread id
 int SetupCallbacks(void)
 {
-	int thid = 0;
-	thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
-	if (thid >= 0)
-		sceKernelStartThread(thid, 0, 0);
-	return thid;
+    int thid = 0;
+    thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
+    if (thid >= 0)
+	sceKernelStartThread(thid, 0, 0);
+    return thid;
 }
 
-/* Utility Savedata functions */
 
-/* based on Shine's function, args: SceUtilitySavedataParam* savedata pass the save data structure
-                       mode: pass the mode parameter 0 = load 1 = save */
+// Graphics stuff, based on cube sample
+static unsigned int __attribute__((aligned(16))) list[262144];
+
+struct Vertex
+{
+    float u, v;
+    unsigned int color;
+    float x, y, z;
+};
+
+struct Vertex __attribute__((aligned(16))) vertices[12*3] =
+{
+    {0, 0, 0xff7f0000,-1,-1, 1}, // 0
+    {1, 0, 0xff7f0000,-1, 1, 1}, // 4
+    {1, 1, 0xff7f0000, 1, 1, 1}, // 5
+    
+    {0, 0, 0xff7f0000,-1,-1, 1}, // 0
+    {1, 1, 0xff7f0000, 1, 1, 1}, // 5
+    {0, 1, 0xff7f0000, 1,-1, 1}, // 1
+    
+    {0, 0, 0xff7f0000,-1,-1,-1}, // 3
+    {1, 0, 0xff7f0000, 1,-1,-1}, // 2
+    {1, 1, 0xff7f0000, 1, 1,-1}, // 6
+    
+    {0, 0, 0xff7f0000,-1,-1,-1}, // 3
+    {1, 1, 0xff7f0000, 1, 1,-1}, // 6
+    {0, 1, 0xff7f0000,-1, 1,-1}, // 7
+    
+    {0, 0, 0xff007f00, 1,-1,-1}, // 0
+    {1, 0, 0xff007f00, 1,-1, 1}, // 3
+    {1, 1, 0xff007f00, 1, 1, 1}, // 7
+    
+    {0, 0, 0xff007f00, 1,-1,-1}, // 0
+    {1, 1, 0xff007f00, 1, 1, 1}, // 7
+    {0, 1, 0xff007f00, 1, 1,-1}, // 4
+    
+    {0, 0, 0xff007f00,-1,-1,-1}, // 0
+    {1, 0, 0xff007f00,-1, 1,-1}, // 3
+    {1, 1, 0xff007f00,-1, 1, 1}, // 7
+    
+    {0, 0, 0xff007f00,-1,-1,-1}, // 0
+    {1, 1, 0xff007f00,-1, 1, 1}, // 7
+    {0, 1, 0xff007f00,-1,-1, 1}, // 4
+    
+    {0, 0, 0xff00007f,-1, 1,-1}, // 0
+    {1, 0, 0xff00007f, 1, 1,-1}, // 1
+    {1, 1, 0xff00007f, 1, 1, 1}, // 2
+    
+    {0, 0, 0xff00007f,-1, 1,-1}, // 0
+    {1, 1, 0xff00007f, 1, 1, 1}, // 2
+    {0, 1, 0xff00007f,-1, 1, 1}, // 3
+    
+    {0, 0, 0xff00007f,-1,-1,-1}, // 4
+    {1, 0, 0xff00007f,-1,-1, 1}, // 7
+    {1, 1, 0xff00007f, 1,-1, 1}, // 6
+    
+    {0, 0, 0xff00007f,-1,-1,-1}, // 4
+    {1, 1, 0xff00007f, 1,-1, 1}, // 6
+    {0, 1, 0xff00007f, 1,-1,-1}, // 5
+};
+
+static void SetupGu()
+{
+    sceGuInit();
+
+    sceGuStart(GU_DIRECT,list);
+    sceGuDrawBuffer(GU_PSM_8888,(void*)0,BUF_WIDTH);
+    sceGuDispBuffer(SCR_WIDTH,SCR_HEIGHT,(void*)0x88000,BUF_WIDTH);
+    sceGuDepthBuffer((void*)0x110000,BUF_WIDTH);
+    sceGuOffset(2048 - (SCR_WIDTH/2),2048 - (SCR_HEIGHT/2));
+    sceGuViewport(2048,2048,SCR_WIDTH,SCR_HEIGHT);
+    sceGuDepthRange(0xc350,0x2710);
+    sceGuScissor(0,0,SCR_WIDTH,SCR_HEIGHT);
+    sceGuEnable(GU_SCISSOR_TEST);
+    sceGuDepthFunc(GU_GEQUAL);
+    sceGuEnable(GU_DEPTH_TEST);
+    sceGuFrontFace(GU_CW);
+    sceGuShadeModel(GU_SMOOTH);
+    sceGuEnable(GU_CULL_FACE);
+    sceGuEnable(GU_CLIP_PLANES);
+    sceGuFinish();
+    sceGuSync(0,0);
+    
+    sceDisplayWaitVblankStart();
+    sceGuDisplay(GU_TRUE);
+}
+
+static void DrawStuff(void)
+{
+    static int val = 0;
+
+    sceGuStart(GU_DIRECT,list);
+
+    // Clear screen    
+    sceGuClearColor(0xff554433);
+    sceGuClearDepth(0);
+    sceGuClear(GU_COLOR_BUFFER_BIT|GU_DEPTH_BUFFER_BIT);
+    
+    // Setup matrices for cube    
+    sceGumMatrixMode(GU_PROJECTION);
+    sceGumLoadIdentity();
+    sceGumPerspective(75.0f,16.0f/9.0f,0.5f,1000.0f);
+    
+    sceGumMatrixMode(GU_VIEW);
+    sceGumLoadIdentity();
+    
+    sceGumMatrixMode(GU_MODEL);
+    sceGumLoadIdentity();
+    {
+		ScePspFVector3 pos = { 0, 0, -2.5f };
+		ScePspFVector3 rot = { val * 0.79f * (M_PI/180.0f), val * 0.98f * (M_PI/180.0f), val * 1.32f * (M_PI/180.0f) };
+		sceGumRotateXYZ(&rot);
+		sceGumTranslate(&pos);
+    }
+    
+    // Draw cube
+    sceGuAmbientColor(0xffffffff);
+    sceGumDrawArray(GU_TRIANGLES,GU_TEXTURE_32BITF|GU_COLOR_8888|GU_VERTEX_32BITF|GU_TRANSFORM_3D,12*3,0,vertices);
+    
+    sceGuFinish();
+    sceGuSync(0,0);
+
+    val ++;
+}
+
+PspUtilitySavedataListSaveNewData newData;
+
+char *titleShow = "New Save";
+
+char nameMultiple[][20] =	// End list with ""
+{
+ "0000",
+ "0001",
+ "0002",
+ "0003",
+ "0004",
+ ""
+};
+
+char key[] = "QTAK319JQKJ952HA";	// Key to encrypt or decrypt savedata
 
 void initSavedata(SceUtilitySavedataParam * savedata, int mode)
 {
 	memset(savedata, 0, sizeof(SceUtilitySavedataParam));
 	savedata->base.size = sizeof(SceUtilitySavedataParam);
 
+	savedata->base.language = PSP_SYSTEMPARAM_LANGUAGE_ENGLISH;
+	savedata->base.buttonSwap = PSP_UTILITY_ACCEPT_CROSS;
 	savedata->base.graphicsThread = 0x11;
 	savedata->base.unknown = 0x13;
 	savedata->base.fontThread = 0x12;
 	savedata->base.soundThread = 0x10;
-	savedata->unknown13 = 1;
-	savedata->mode = mode;		/* mode: 0 = load, 1 = save */
 
-	strcpy(savedata->gameName, gameName);	/* first part of the save name, game identifier name */
-	strcpy(savedata->saveName, saveName);	/* second part of the save name, save identifier name */
-	strcpy(savedata->fileName, fileName);	/* name of the data file */
+	savedata->mode = mode;
+	savedata->overwrite = 1;
+	savedata->focus = PSP_UTILITY_SAVEDATA_FOCUS_LATEST; // Set initial focus to the newest file (for loading)
 
-	/* Allocate buffers used to store various parts of the save data. */
+#if _PSP_FW_VERSION >= 200
+	strncpy(savedata->key, key, 16);
+#endif
+
+	strcpy(savedata->gameName, "DEMO11111");	// First part of the save name, game identifier name
+	strcpy(savedata->saveName, "0000");	// Second part of the save name, save identifier name
+
+	// List of multiple names
+	savedata->saveNameList = nameMultiple;
+
+	strcpy(savedata->fileName, "DATA.BIN");	// name of the data file
+
+	// Allocate buffers used to store various parts of the save data
 	savedata->dataBuf = malloc(DATABUFFLEN);
 	savedata->dataBufSize = DATABUFFLEN;
+	savedata->dataSize = DATABUFFLEN;
 
-	savedata->icon0FileData.buf = malloc(READICON0LEN);
-	savedata->icon0FileData.bufSize = READICON0LEN;
-	savedata->icon1FileData.buf = malloc(READICON1LEN);
-	savedata->icon1FileData.bufSize = READICON1LEN;
-	savedata->pic1FileData.buf = malloc(READPIC1LEN);
-	savedata->pic1FileData.bufSize = READPIC1LEN;
+	// Set some data
+	if (mode == PSP_UTILITY_SAVEDATA_LISTSAVE)
+	{
+	 memset(savedata->dataBuf, 0, DATABUFFLEN);
+	 strcpy(savedata->dataBuf,"score: 10");
+
+	strcpy(savedata->sfoParam.title,"Title");
+	strcpy(savedata->sfoParam.savedataTitle,"Title 2");
+	strcpy(savedata->sfoParam.detail,"Details");
+	savedata->sfoParam.parentalLevel = 1;
+	
+	// No icon1
+	savedata->icon1FileData.buf = NULL;
+	savedata->icon1FileData.bufSize = 0;
+	savedata->icon1FileData.size = 0;
+
+	savedata->pic1FileData.buf = pic1;
+	savedata->pic1FileData.bufSize = size_pic1;
+	savedata->pic1FileData.size = size_pic1;
+
+	savedata->icon0FileData.buf = icon0;
+	savedata->icon0FileData.bufSize = size_icon0;
+	savedata->icon0FileData.size = size_icon0;
+	
+	// No snd0
+	savedata->snd0FileData.buf = NULL;
+	savedata->snd0FileData.bufSize = 0;
+	savedata->snd0FileData.size = 0;
+
+	// Set title
+	newData.title = titleShow;
+
+	// Set new data
+	savedata->newData = &newData;
+	
+	savedata->focus = PSP_UTILITY_SAVEDATA_FOCUS_FIRSTEMPTY; // If saving, set inital focus to the first empty slot
+	}
 }
 
-void WorkSaveData(int mode)
+static void ShowSaveDialog (int mode)
 {
-	SceUtilitySavedataParam savedata;
+	SceUtilitySavedataParam dialog;
+	
+    initSavedata(&dialog, mode);
+	
+    sceUtilitySavedataInitStart(&dialog);
 
-	initSavedata(&savedata, mode);
+    while(running) {
 
-	int fd_data = sceIoOpen("ms0:/dataBuf", PSP_O_TRUNC | PSP_O_CREAT | PSP_O_WRONLY, 0777);
-	int fd_sfo = sceIoOpen("ms0:/sfodata", PSP_O_TRUNC | PSP_O_CREAT | PSP_O_WRONLY, 0777);
+	DrawStuff();
 
-	/* If we are writing savedata, then load files and put their contents into the savedata structure. */
-	if (mode == 1)				
-	{
-		sceIoRead(fd_data, savedata.dataBuf, DATABUFFLEN);
-		sceIoRead(fd_sfo, &savedata.sfoParam.title, sizeof(savedata.sfoParam.title));
-		sceIoRead(fd_sfo, &savedata.sfoParam.savedataTitle, sizeof(savedata.sfoParam.savedataTitle));
-		sceIoRead(fd_sfo, &savedata.sfoParam.detail, sizeof(savedata.sfoParam.detail));
-		sceIoClose(fd_data);
-		sceIoClose(fd_sfo);
+	switch(sceUtilitySavedataGetStatus()) {
+
+	case PSP_UTILITY_DIALOG_VISIBLE :
+
+	    sceUtilitySavedataUpdate(1);
+	    break;
+
+	case PSP_UTILITY_DIALOG_QUIT :
+
+	    sceUtilitySavedataShutdownStart();
+	    break;
+	    
+	case PSP_UTILITY_DIALOG_FINISHED :
+		if(mode == PSP_UTILITY_SAVEDATA_LISTSAVE)
+			sprintf(string, "Saved: score = 10");
+		else
+			sprintf(string, "Loaded: %s\n", (char*)dialog.dataBuf); // The actual data in the savedata file that was saved previously
+			
+	case PSP_UTILITY_DIALOG_NONE :
+	    return;
 	}
 
-	int result = sceUtilitySavedataInitStart(&savedata);
-	if (result)
-		printf("sceUtilitySavedataInitStart failed: %i\n", result);
-
-	/* Wait for sceUtilitySavedataInitStart() to complete. */
-	while (1)					
-	{
-		result = sceUtilitySavedataGetStatus();
-		if (result == 3)
-			break;
-		sceUtilitySavedataUpdate(1);
-		sceDisplayWaitVblankStart();
-	}
-
-	/* Shutdown the Savedata Utility. */
-	result = sceUtilitySavedataShutdownStart();	
-	if (result)
-		printf("sceUtilitySavedataShutdownStart() failed: %i", result);
-
-	/* Wait for sceUtilitySavedataShutdownStart() to complete. */
-	while (1)					
-	{
-		result = sceUtilitySavedataGetStatus();
-		if (result == 4)
-			break;
-		sceUtilitySavedataUpdate(1);
-		sceDisplayWaitVblankStart();
-	}
-
-	/* If we are reading save data then write it to a file in the MS root directory. */
-	if (mode == 0)
-	{
-		sceIoWrite(fd_data, savedata.dataBuf, DATABUFFLEN);
-		sceIoWrite(fd_sfo, &savedata.sfoParam.title, sizeof(savedata.sfoParam.title));
-		sceIoWrite(fd_sfo, &savedata.sfoParam.savedataTitle, sizeof(savedata.sfoParam.savedataTitle));
-		sceIoWrite(fd_sfo, &savedata.sfoParam.detail, sizeof(savedata.sfoParam.detail));
-		sceIoClose(fd_data);
-		sceIoClose(fd_sfo);
-	}
+	sceDisplayWaitVblankStart();
+	sceGuSwapBuffers();
+    }
 }
 
-/* main routine */
 int main(int argc, char *argv[])
 {
+    SceCtrlData pad;
+	
 	SetupCallbacks();
+    SetupGu();
+	
 	pspDebugScreenInit();
-
-	printf("sceUtilitySavedata sample\n\n"
-		   "Press Circle to load savedata, and put it into ms0:/\n"
-		   "Press Cross to save savedata, from files put into ms0:/\n"
-		   "Press Triangle to exit.\n\n");
-
-	while (1)
-	{
-		SceCtrlData pad;
-
+	
+	int mode = 0;
+	
+	memset(string, 0, sizeof(string));
+	
+	while(running)
+	{		
+		pspDebugScreenSetXY(0, 0);
+	
+		pspDebugScreenPrintf("Press X to Save, O to Load\n");
+		pspDebugScreenPrintf(string);
+		
 		sceCtrlReadBufferPositive(&pad, 1);
-		if (pad.Buttons & PSP_CTRL_CIRCLE) {
-			WorkSaveData(0);
+		
+		if(pad.Buttons & PSP_CTRL_CROSS)
+			mode = PSP_UTILITY_SAVEDATA_LISTSAVE;
+		
+		else if(pad.Buttons & PSP_CTRL_CIRCLE)
+			mode = PSP_UTILITY_SAVEDATA_LISTLOAD;
+		
+		if(mode)
+		{
+			ShowSaveDialog(mode);
+			pspDebugScreenInit();
+			mode = 0;
 		}
-		if (pad.Buttons & PSP_CTRL_CROSS) {
-			WorkSaveData(1);
-		}
-		if (pad.Buttons & PSP_CTRL_TRIANGLE)
-			break;
 	}
 
-	sceKernelExitGame();
-	return 0;
+    sceKernelExitGame();
+	
+    return 0;
 }
--- pspsdk/src/utility/psputility_savedata.h
+++ pspsdk/src/utility/psputility_savedata.h
@@ -9,6 +9,7 @@
  * Copyright (c) 2005    Shine
  *                       weltall <weltall@consoleworld.org>
  *                       Marcus R. Brown <mrbrown@ocgnet.org>
+ *						 InsertWittyName <tias_dp@hotmai.com>
  *
  * $Id$
  */
@@ -22,6 +23,36 @@
 extern "C" {
 #endif
 
+/** Save data utility modes */
+typedef enum
+{
+	PSP_UTILITY_SAVEDATA_AUTOLOAD = 0,
+	PSP_UTILITY_SAVEDATA_AUTOSAVE,
+	PSP_UTILITY_SAVEDATA_LOAD,
+	PSP_UTILITY_SAVEDATA_SAVE,
+	PSP_UTILITY_SAVEDATA_LISTLOAD,
+	PSP_UTILITY_SAVEDATA_LISTSAVE,
+	PSP_UTILITY_SAVEDATA_LISTDELETE,
+	PSP_UTILITY_SAVEDATADELETE,
+
+} PspUtilitySavedataMode;
+
+/** Initial focus position for list selection types */
+typedef enum
+{
+	PSP_UTILITY_SAVEDATA_FOCUS_UNKNOWN = 0,
+	PSP_UTILITY_SAVEDATA_FOCUS_FIRSTLIST,	/* First in list */
+	PSP_UTILITY_SAVEDATA_FOCUS_LASTLIST,	/* Last in list */
+	PSP_UTILITY_SAVEDATA_FOCUS_LATEST,	/* Most recent date */
+	PSP_UTILITY_SAVEDATA_FOCUS_OLDEST,	/* Oldest date */
+	PSP_UTILITY_SAVEDATA_FOCUS_UNKNOWN2,
+	PSP_UTILITY_SAVEDATA_FOCUS_UNKNOWN3,
+	PSP_UTILITY_SAVEDATA_FOCUS_FIRSTEMPTY, /* First empty slot */
+	PSP_UTILITY_SAVEDATA_FOCUS_LASTEMPTY,	/*Last empty slot */
+	
+} PspUtilitySavedataFocus;
+
+
 /** title, savedataTitle, detail: parts of the unencrypted SFO
     data, it contains what the VSH and standard load screen shows */
 typedef struct PspUtilitySavedataSFOParam
@@ -31,6 +62,7 @@ typedef struct PspUtilitySavedataSFOParam
 	char detail[0x400];
 	unsigned char parentalLevel;
 	unsigned char unknown[3];
+	
 } PspUtilitySavedataSFOParam;
 
 typedef struct PspUtilitySavedataFileData {
@@ -38,27 +70,39 @@ typedef struct PspUtilitySavedataFileData {
 	SceSize bufSize;
 	SceSize size;	/* ??? - why are there two sizes? */
 	int unknown;
+	
 } PspUtilitySavedataFileData;
 
-/** Structure to hold the parameters for the ::sceUtilitySavedataInitStart function.
-  */
+typedef struct PspUtilitySavedataListSaveNewData
+{
+	PspUtilitySavedataFileData icon0;
+	char *title;
+	
+} PspUtilitySavedataListSaveNewData;
+
+/** Structure to hold the parameters for the ::sceUtilitySavedataInitStart function. */
 typedef struct SceUtilitySavedataParam
 {
 	pspUtilityDialogCommon base;
 
-	/** mode: 0 to load, 1 to save */
-	int mode;
-	int unknown12;
-
-	/** unknown13 use 0x10 */
-	int unknown13;
+	PspUtilitySavedataMode mode;
+	
+	int unknown1;
+	
+	int overwrite;
 
 	/** gameName: name used from the game for saves, equal for all saves */
-	char gameName[16];
+	char gameName[13];
+	char reserved[3];
 	/** saveName: name of the particular save, normally a number */
-	char saveName[24];
+	char saveName[20];
+
+	/** saveNameList: used by multiple modes */
+	char (*saveNameList)[20];
+
 	/** fileName: name of the data file of the game for example DATA.BIN */
-	char fileName[16];
+	char fileName[13];
+	char reserved1[3];
 
 	/** pointer to a buffer that will contain data file unencrypted data */
 	void *dataBuf;
@@ -73,7 +117,25 @@ typedef struct SceUtilitySavedataParam
 	PspUtilitySavedataFileData pic1FileData;
 	PspUtilitySavedataFileData snd0FileData;
 
-	unsigned char unknown17[4];
+	/** Pointer to an PspUtilitySavedataListSaveNewData structure */
+	PspUtilitySavedataListSaveNewData *newData;
+
+	/** Initial focus for lists */
+	PspUtilitySavedataFocus focus;
+
+	/** unknown2: ? */
+	int unknown2[4];
+
+#if _PSP_FW_VERSION >= 200
+
+	/** key: encrypt/decrypt key for save with firmware >= 2.00 */
+	char key[16];
+
+	/** unknown3: ? */
+	char unknown3[20];
+
+#endif
+
 } SceUtilitySavedataParam;
 
 
