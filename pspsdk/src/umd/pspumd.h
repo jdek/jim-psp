51f8b1df73020a736c51514b77e36122889cebdf
iwn <iwn@fd6133d2-eef9-0310-a487-e40d4489b40b>
2008-05-24 16:24:05 +0000

    Added and fixed various sceUmd* functions. sceUmdUser should now be complete.

--- pspsdk/src/umd/pspumd.h
+++ pspsdk/src/umd/pspumd.h
@@ -21,13 +21,42 @@ extern "C" {
 /** @addtogroup UMD UMD Kernel Library */
 /*@{*/
 
-/** Enumeration for UMD stats */
+/** UMD Info struct */
+typedef struct pspUmdInfo
+{
+	/** Set to sizeof(pspUmdInfo) */
+	unsigned int size;
+	/** One or more of ::pspUmdTypes */
+	unsigned int type;
+	
+} pspUmdInfo;
+
+/** Enumeration for UMD types */
+enum pspUmdTypes
+{
+	PSP_UMD_TYPE_GAME =		0x10,
+	PSP_UMD_TYPE_VIDEO =	0x20,
+	PSP_UMD_TYPE_AUDIO =	0x40
+};
+
+/** Enumeration for UMD drive state */
+enum pspUmdState
+{
+	PSP_UMD_NOT_PRESENT =	0x01,
+	PSP_UMD_PRESENT =		0x02,
+	PSP_UMD_CHANGED =		0x04,
+	PSP_UMD_INITING =		0x08,
+	PSP_UMD_INITED =		0x10,
+	PSP_UMD_READY =			0x20
+};
+
+/** Enumeration for UMD stats (legacy) */
 enum UmdDriveStat
 {
 	/** Wait for disc to be inserted */
-	UMD_WAITFORDISC = 2,
+	UMD_WAITFORDISC = PSP_UMD_PRESENT,
 	/** Wait for the UMD to be initialised so it can be accessed from the mapped drive */
-	UMD_WAITFORINIT		= 0x20
+	UMD_WAITFORINIT	= PSP_UMD_READY
 };
 
 /** UMD Callback function */
@@ -36,32 +65,41 @@ typedef int (*UmdCallback)(int unknown, int event);
 /** 
   * Check whether there is a disc in the UMD drive
   *
-  * @param a - Unknown function, set to 0
-  * @return Returns 0 if no disc present, anything else indicates a disc is inserted.
+  * @return 0 if no disc present, anything else indicates a disc is inserted.
   */
-int sceUmdCheckMedium(int a);
+int sceUmdCheckMedium(void);
+
+/** 
+  * Get the disc info
+  *
+  * @param info - A pointer to a ::pspUmdInfo struct
+  *
+  * @return < 0 on error
+  */
+int sceUmdGetDiscInfo(pspUmdInfo *info);
 
 /** 
   * Activates the UMD drive
   * 
   * @param unit - The unit to initialise (probably). Should be set to 1.
+  *
   * @param drive - A prefix string for the fs device to mount the UMD on (e.g. "disc0:")
+  *
   * @return < 0 on error
   *
   * @par Example:
   * @code
   * // Wait for disc and mount to filesystem
   * int i;
-  * i = sceUmdCheckMedium(0);
+  * i = sceUmdCheckMedium();
   * if(i == 0)
   * {
-  *    sceUmdWaitDriveStat(UMD_WAITFORDISC);
+  *    sceUmdWaitDriveStat(PSP_UMD_PRESENT);
   * }
   * sceUmdActivate(1, "disc0:"); // Mount UMD to disc0: file system
-  * sceUmdWaitDriveStat(UMD_WAITFORINIT);
+  * sceUmdWaitDriveStat(PSP_UMD_READY);
   * // Now you can access the UMD using standard sceIo functions
   * @endcode
-  *
   */
 int sceUmdActivate(int unit, const char *drive);
 
@@ -69,23 +107,67 @@ int sceUmdActivate(int unit, const char *drive);
   * Deativates the UMD drive
   * 
   * @param unit - The unit to initialise (probably). Should be set to 1.
-  * @param drive - A prefix string for the fs device to mount the UMD on (e.g. "disc0:")
-  * @return < 0 on error
   *
+  * @param drive - A prefix string for the fs device to mount the UMD on (e.g. "disc0:")
+  *
+  * @return < 0 on error
   */
 int sceUmdDeactivate(int unit, const char *drive);
 
 /** 
-  * Wait for a drive to reach a certain state
+  * Wait for the UMD drive to reach a certain state
   *
-  * @param stat - The drive stat to wait for.
+  * @param stat - One or more of ::pspUmdState
+  *
   * @return < 0 on error
-  *
   */
 int sceUmdWaitDriveStat(int stat);
 
 /** 
-  * Register a callback for the UMD drive.
+  * Wait for the UMD drive to reach a certain state
+  *
+  * @param stat - One or more of ::pspUmdState
+  *
+  * @param timeout - Timeout value in microseconds
+  *
+  * @return < 0 on error
+  */
+int sceUmdWaitDriveStatWithTimer(int stat, unsigned int timeout);
+
+/** 
+  * Wait for the UMD drive to reach a certain state (plus callback)
+  *
+  * @param stat - One or more of ::pspUmdState
+  *
+  * @param timeout - Timeout value in microseconds
+  *
+  * @return < 0 on error
+  */
+int sceUmdWaitDriveStatCB(int stat, unsigned int timeout);
+
+/** 
+  * Cancel a sceUmdWait* call
+  *
+  * @return < 0 on error
+  */
+int sceUmdCancelWaitDriveStat(void);
+
+/** 
+  * Get (poll) the current state of the UMD drive
+  *
+  * @return < 0 on error, one or more of ::pspUmdState on success
+  */
+int sceUmdGetDriveStat(void);
+
+/** 
+  * Get the error code associated with a failed event
+  *
+  * @return < 0 on error, the error code on success
+  */
+int sceUmdGetErrorStat(void);
+
+/** 
+  * Register a callback for the UMD drive
   * @note Callback is of type UmdCallback
   *
   * @param cbid - A callback ID created from sceKernelCreateCallback
@@ -97,6 +179,29 @@ int sceUmdWaitDriveStat(int stat);
   * @endcode
   */
 int sceUmdRegisterUMDCallBack(int cbid);
+
+/** 
+  * Un-register a callback for the UMD drive
+  *
+  * @param cbid - A callback ID created from sceKernelCreateCallback
+  *
+  * @return < 0 on error
+  */
+int sceUmdUnRegisterUMDCallBack(int cbid);
+
+/** 
+  * Permit UMD disc being replaced
+  *
+  * @return < 0 on error
+  */
+int sceUmdReplacePermit(void);
+
+/** 
+  * Prohibit UMD disc being replaced
+  *
+  * @return < 0 on error
+  */
+int sceUmdReplaceProhibit(void);
 
 /*@}*/
 
