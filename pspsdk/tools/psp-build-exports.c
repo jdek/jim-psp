e8f780d225d63dec81843dea795ab53445a50017
Raphael <Raphael@fd6133d2-eef9-0310-a487-e40d4489b40b>
2008-08-01 16:56:26 +0000

    applied patch by SilverSpring http://forums.ps2dev.org/viewtopic.php?t=10757

--- pspsdk/tools/psp-build-exports.c
+++ pspsdk/tools/psp-build-exports.c
@@ -335,7 +335,7 @@ void build_exports_output_extern(struct psp_export *pHead)
 	pExp = pHead;
 	while(pExp != NULL)
 	{
-		fprintf(stdout, "void extern %s;\n", pExp->name);
+		fprintf(stdout, "extern void %s;\n", pExp->name);
 		pExp = pExp->pNext;
 	}
 }
