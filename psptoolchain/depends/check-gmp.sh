5016a58f794350979c5a0fd86acf0562a9c814f4
oopo <oopo@fd6133d2-eef9-0310-a487-e40d4489b40b>
2008-08-24 18:07:58 +0000

    Argh, removing these files for a moment.

--- psptoolchain/depends/check-gmp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# check-gmp.sh by Dan Peori (danpeori@oopo.net)
-
- ## Check for the gmp library.
- ls /usr/include/gmp.h 1> /dev/null || { echo "ERROR: Install gmp before continuing."; exit 1; }
