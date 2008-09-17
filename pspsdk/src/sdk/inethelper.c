af1e860a6b6ea676cd19205d767442c587f279d3
jim <jim@fd6133d2-eef9-0310-a487-e40d4489b40b>
2008-09-17 19:44:47 +0000

    Bump up sceNetApctlInit stack size again, see http://forums.ps2dev.org/viewtopic.php?p=74345#74345

--- pspsdk/src/sdk/inethelper.c
+++ pspsdk/src/sdk/inethelper.c
@@ -79,7 +79,7 @@ int pspSdkInetInit()
 	if (retVal != 0)
 		return retVal;
 	
-	retVal = sceNetApctlInit(0x1400, 0x42);
+	retVal = sceNetApctlInit(0x1600, 0x42);
 	if (retVal != 0)
 		return retVal;
 
