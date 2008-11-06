9098044a242b9a34655c64932c3895d0abac217b
oopo <oopo@fd6133d2-eef9-0310-a487-e40d4489b40b>
2008-11-06 14:22:30 +0000

    Updated to newlib-1.16.0 and gcc-4.3.2. by Heimdall and CHERTS http://forums.ps2dev.org/viewtopic.php?t=11236 http://forums.ps2dev.org/viewtopic.php?t=11226

--- psptoolchain/scripts/004-newlib-1.16.0.sh
+++ psptoolchain/scripts/004-newlib-1.16.0.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# newlib-1.15.0.sh by Dan Peori (danpeori@oopo.net)
+# newlib-1.16.0.sh by Dan Peori (danpeori@oopo.net)
 
  ## Download the source code.
- wget --continue ftp://sources.redhat.com/pub/newlib/newlib-1.15.0.tar.gz || { exit 1; }
+ wget --continue ftp://sources.redhat.com/pub/newlib/newlib-1.16.0.tar.gz || { exit 1; }
 
  ## Unpack the source code.
- rm -Rf newlib-1.15.0 && tar xfvz newlib-1.15.0.tar.gz || { exit 1; }
+ rm -Rf newlib-1.16.0 && tar xfvz newlib-1.16.0.tar.gz || { exit 1; }
 
  ## Enter the source directory and patch the source code.
- cd newlib-1.15.0 && cat ../../patches/newlib-1.15.0-PSP.patch | patch -p1 || { exit 1; }
+ cd newlib-1.16.0 && cat ../../patches/newlib-1.16.0-PSP.patch | patch -p1 || { exit 1; }
 
  ## Create and enter the build directory.
  mkdir build-psp && cd build-psp || { exit 1; }
