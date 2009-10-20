5edf10383db53e73f78698ae97d175bc657da49e
jim <jim@fd6133d2-eef9-0310-a487-e40d4489b40b>
2009-10-20 21:57:04 +0000

    Fix buffer overflow in psp-ar, as detected by GCC's Fortify Source feature

--- psptoolchain/scripts/001-binutils-2.16.1.sh
+++ psptoolchain/scripts/001-binutils-2.16.1.sh
@@ -8,13 +8,15 @@
  rm -Rf binutils-2.16.1 && tar xfvj binutils-2.16.1.tar.bz2 || { exit 1; }
 
  ## Enter the source directory and patch the source code.
- cd binutils-2.16.1 && cat ../../patches/binutils-2.16.1-PSP.patch | patch -p1 || { exit 1; }
+ cd binutils-2.16.1 || { exit 1; }
+ patch -p 1 < ../../patches/binutils-2.16.1-PSP.patch || { exit 1; }
+ patch -p 0 < ../../patches/binutils-2.16.1-fix-ar.patch || { exit 1; }
 
  ## Create and enter the build directory.
  mkdir build-psp && cd build-psp || { exit 1; }
 
  ## Configure the build.
- CFLAGS="-O0" ../configure --prefix="$PSPDEV" --target="psp" --enable-install-libbfd || { exit 1; }
+ ../configure --prefix="$PSPDEV" --target="psp" --enable-install-libbfd || { exit 1; }
 
  ## Compile and install. ( -r is required for building under osx )
  make clean && make -r -j 2 && make install && make clean || { exit 1; }
